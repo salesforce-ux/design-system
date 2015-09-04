ae96f80703ac0c0225958a631a380d2dbd990aa1
Richard Boardman <rboardman@salesforce.com>
2015-09-03 17:38:26 -0700

    landmark purge

--- scripts/test/test.js
+++ scripts/test/test.js
@@ -9,7 +9,7 @@ Neither the name of salesforce.com, inc. nor the names of its contributors may b
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-// landmark test runner
+// SLDS test runner
 // command line args:
 //   --runfrom [local | sauce] : where to run tests FROM'
 //   --testserver [local | prod | stage | test] : where to run tests ON'
@@ -143,7 +143,7 @@ before(function(done) {
       withCapabilities({
           browserName: browserTarget,
           platform: osTarget,
-          name: 'Landmark test: ' + testsToRun + ' on ' + testserver + ' using ' + osTarget + '/' +browserTarget,
+          name: 'SLDS test: ' + testsToRun + ' on ' + testserver + ' using ' + osTarget + '/' +browserTarget,
           username: process.env.SAUCE_USERNAME,
           accessKey: process.env.SAUCE_ACCESS_KEY
       }).

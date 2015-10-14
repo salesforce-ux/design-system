bfc5addd8d52d65632ebe4f64c8f749d7149e0c4
Adam Putinski <adam.putinski@gmail.com>
2015-10-14 16:59:08 -0400

    Remove test branch

--- scripts/travis/after_success.sh
+++ scripts/travis/after_success.sh
@@ -3,6 +3,6 @@ set -e
 
 if [ -n "$DEPLOY_SCRIPTS_REPO" ]; then
   rm -rf scripts-internal
-  git clone $DEPLOY_SCRIPTS_REPO -b server-refactor scripts-internal
+  git clone $DEPLOY_SCRIPTS_REPO scripts-internal
   ./node_modules/.bin/babel-node scripts-internal/deploy/heroku-ci.js
 fi

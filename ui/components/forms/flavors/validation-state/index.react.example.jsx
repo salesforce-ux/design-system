dabaceae31826794e97a955b6fc803b6b4aae10f
Brandon Ferrua <bferrua@gmail.com>
2015-11-05 11:38:25 -0800

    revert actionable button to just an icon

--- ui/components/forms/flavors/validation-state/index.react.example.jsx
+++ ui/components/forms/flavors/validation-state/index.react.example.jsx
@@ -31,11 +31,7 @@ module.exports = (
       <label className={pf('form-element__label')} htmlFor="email-0-2">Input Error with Icon Left</label>
       <div className={pf('form-element__control')}>
         <div className={pf('input-has-icon input-has-icon--left')}>
-          <a href="javascript:void(0);" className={pf('button button--icon-bare input__icon')}>
-            <SvgIcon className={pf('button__icon')} sprite="utility"
-            symbol="warning" />
-            <span className={pf('assistive-text')}>Warning</span>
-          </a>
+          <SvgIcon className={pf('icon input__icon')} sprite="utility" symbol="warning" />
           <input id="email-0-2" className={pf('input')} type="email" aria-describedby="errorSample1" required />
         </div>
         <span id="errorSample1" className={pf('form-element__help')}>This field is required</span>

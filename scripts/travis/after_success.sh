12fa53cdfb5997b59457c441848fcd19761e75a8
Adam Putinski <adam.putinski@gmail.com>
2015-11-17 16:36:30 -0500

    Prepare for mulit-site deployments

--- scripts/travis/after_success.sh
+++ scripts/travis/after_success.sh
@@ -2,7 +2,5 @@
 set -e
 
 if [ -n "$DEPLOY_SCRIPTS_REPO" ]; then
-  rm -rf scripts-internal
-  git clone $DEPLOY_SCRIPTS_REPO scripts-internal
   ./node_modules/.bin/babel-node scripts-internal/deploy/heroku-ci.js
 fi
--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e
-if [ X"$TRAVIS_SECURE_ENV_VARS" = X"true" ]; then
-npm login <<!
+
+if [[ "$TRAVIS_SECURE_ENV_VARS" = "true" ]]; then
+  echo "TRAVIS: Logging into NPM"
+  npm login <<!
 ${NPM_USERNAME}
 ${NPM_PASSWORD}
 ${NPM_EMAIL}
 !
 else
   echo "TRAVIS: TRAVIS_SECURE_ENV_VARS env variable not set to true"
+fi
+
+if [ -n "$DEPLOY_SCRIPTS_REPO" ]; then
+  rm -rf scripts-internal
+  git clone $DEPLOY_SCRIPTS_REPO -b multiple-sites scripts-internal
 fi

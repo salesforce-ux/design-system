52680fb626ca18eba218b50e129293e3e8576779
Adam Putinski <adam.putinski@gmail.com>
2015-12-15 10:50:56 -0500

    Remove "deploy" script

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -7,11 +7,3 @@ npm run lint || true
 npm run prod
 npm run test-after
 npm run dist
-
-# Deployment
-if [ -n "$DEPLOY_SCRIPTS_REPO" ]; then
-  echo "TRAVIS: running heroku deployment"
-  npm run deploy
-else
-  echo "TRAVIS: skipping heroku deployment"
-fi

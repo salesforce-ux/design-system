5cb8ad673c832482f644428c5330a05bb3ec54dc
Kaelig <kaelig@deloumeau.fr>
2015-12-22 14:35:29 +0000

    Fix JS linting configuration

--- scripts/lint.js
+++ scripts/lint.js
@@ -36,7 +36,7 @@ gulp.task('lint:spaces', () =>
     'ui/**/*.*',
     'site/**/*.{js,jsx,sh,scss,yml,md,xml}',
     'scripts/**/*.{js,sh,jsx}'
-  ], { cwd: __PATHS__.root })
+  ])
   .pipe(lintspaces({
     editorconfig: '.editorconfig',
     ignores: [
@@ -48,12 +48,12 @@ gulp.task('lint:spaces', () =>
 
 gulp.task('lint:js', () =>
   gulp.src([
-    '*.{js}',
+    '*.js',
     'app_modules/**/*.{js,jsx}',
     'scripts/**/*.{js,jsx}',
     'site/**/*.{js,jsx}',
     'ui/**/*.{js,jsx}'
-  ], { cwd: __PATHS__.root })
+  ])
   // eslint() attaches the lint output to the "eslint" property
   // of the file object so it can be used by other modules.
   .pipe(eslint())

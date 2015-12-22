ce6c3e0cf602029e6906ac62bde6c7a302a35d44
Kaelig <kaelig@deloumeau.fr>
2015-12-22 14:16:41 +0000

    Add gulp webpack and Sass watchers

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,9 +9,65 @@ Neither the name of salesforce.com, inc. nor the names of its contributors may b
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import webpack from 'webpack';
+import sass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+
+
+import path from 'path';
 import './scripts/helpers/setup';
 import './scripts/lint';
+import { getConfig as webpackConfig } from './scripts/tasks/site/webpack';
 
-import gulp from 'gulp';
+const webpackCompiler = webpack(webpackConfig());
+
+gulp.task('site:webpack', (callback) => {
+  webpackCompiler.run(function(err, stats) {
+    if (err) {
+      throw new gutil.PluginError("webpack:build-dev", err);
+    }
+    gutil.log("[site:webpack]", stats.toString({
+      colors: true
+    }));
+    callback();
+  })
+});
+
+gulp.task('site:sass', () => {
+  gulp.src(path.resolve(__PATHS__.site, 'assets/styles/*.scss'))
+    .pipe(sass({
+      outputStyle: 'compressed',
+      sourceComments: true,
+      includePaths: [
+        __PATHS__.root,
+        __PATHS__.ui,
+        __PATHS__.node_modules
+      ]
+    }).on('error', sass.logError))
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+    .pipe(gulp.dest('./.www/assets/styles'));
+});
+
+
+gulp.task('watch:webpack', () => {
+  gulp.watch([
+    'app_modules/**/*.{js,jsx}',
+    'ui/**/*.{js,jsx}',
+    'site/**/*.{js,jsx}'
+  ], ['site:webpack'])
+});
+
+gulp.task('watch:sass', () => {
+  gulp.watch([
+    path.resolve(__PATHS__.site, 'assets/styles/**/*.scss'),
+    path.resolve(__PATHS__.ui, '**/*.scss')
+  ], ['site:sass'])
+});
+
+gulp.task('watch', ['watch:sass', 'watch:webpack']);
 
 gulp.task('default', ['lint']);
+

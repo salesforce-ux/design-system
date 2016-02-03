b497f0929fcdcc2dee5e7a6f99661a6c8b267396
Adam Putinski <adam.putinski@gmail.com>
2016-02-03 13:31:47 -0500

    Typo

--- scripts/gulp/pages.jsx
+++ scripts/gulp/pages.jsx
@@ -308,7 +308,7 @@ export const gulpRenderComponentPage = () =>
       const componentPath = /^utilities/.test(component.path)
         ? `components/${component.path}`
         : component.path;
-      // Push a new HTML pagea
+      // Push a new HTML page
       next(null, new gutil.File({
         path: path.resolve(__PATHS__.site, componentPath, 'index.html'),
         contents: new Buffer(renderPage(pageBody)),

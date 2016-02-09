fd1d732102ff38beff775bf14d65f4e59d4bc584
Stephanie Rewis <srewis@salesforce.com>
2016-02-09 12:54:03 -0800

    Remove left padding on buttons

--- ui/components/trees/flavors/grid/index.react.example.jsx
+++ ui/components/trees/flavors/grid/index.react.example.jsx
@@ -42,7 +42,7 @@ export default (
     </thead>
     <tbody>
     <tr className={pf('hint-parent')} id="tree0-node0" role="row" aria-level="1" aria-expanded="true">
-      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node0" className={pf('m-right--x-small m-left--xx-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>United Partners</a></th>
+      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node0" className={pf('m-right--x-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>United Partners</a></th>
       <td data-label="employees"><a href="#" className={pf('truncate')}>10,000</a></td>
       <td data-label="phone-number"><span className={pf('truncate')}>837-555-1212</span></td>
       <td data-label="account-owner"><span className={pf('truncate')}>John Doe</span></td>
@@ -57,7 +57,7 @@ export default (
       </td>
     </tr>
     <tr className={pf('hint-parent')} id="tree0-node1" role="row" aria-level="2" aria-expanded="true">
-      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node1" className={pf('m-right--x-small m-left--xx-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>United Partners (Bay Area)</a></th>
+      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node1" className={pf('m-right--x-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>United Partners (Bay Area)</a></th>
       <td data-label="employees"><a href="#" className={pf('truncate')}>3,000</a></td>
       <td data-label="phone-number"><span className={pf('truncate')}>837-555-1212</span></td>
       <td data-label="account-owner"><span className={pf('truncate')}>John Doe</span></td>
@@ -87,7 +87,7 @@ export default (
       </td>
     </tr>
     <tr className={pf('hint-parent')} id="tree0-node2" role="row" aria-level="2" aria-expanded="true">
-      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2" className={pf('m-right--x-small m-left--xx-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>United Partners (San Francisco)</a></th>
+      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2" className={pf('m-right--x-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>United Partners (San Francisco)</a></th>
       <td data-label="employees"><a href="#" className={pf('truncate')}>578</a></td>
       <td data-label="phone-number"><span className={pf('truncate')}>837-555-1212</span></td>
       <td data-label="account-owner"><span className={pf('truncate')}>Jane Doe</span></td>
@@ -102,7 +102,7 @@ export default (
       </td>
     </tr>
     <tr className={pf('hint-parent')} id="tree0-node2-0" role="row" aria-level="3" aria-expanded="true">
-      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0" className={pf('m-right--x-small m-left--xx-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>United Partners (East)</a></th>
+      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0" className={pf('m-right--x-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>United Partners (East)</a></th>
       <td data-label="employees"><a href="#" className={pf('truncate')}>430</a></td>
       <td data-label="phone-number"><span className={pf('truncate')}>837-555-1212</span></td>
       <td data-label="account-owner"><span className={pf('truncate')}>John Doe</span></td>
@@ -117,7 +117,7 @@ export default (
       </td>
     </tr>
     <tr className={pf('hint-parent')} id="tree0-node2-0-0" role="row" aria-level="4" aria-expanded="true">
-      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0" className={pf('m-right--x-small m-left--xx-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>United Partners (VA)</a></th>
+      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0" className={pf('m-right--x-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>United Partners (VA)</a></th>
       <td data-label="employees"><a href="#" className={pf('truncate')}>410</a></td>
       <td data-label="phone-number"><span className={pf('truncate')}>837-555-1212</span></td>
       <td data-label="account-owner"><span className={pf('truncate')}>John Doe</span></td>
@@ -132,7 +132,7 @@ export default (
       </td>
     </tr>
     <tr className={pf('hint-parent')} id="tree0-node2-0-0-0" role="row" aria-level="5" aria-expanded="true">
-      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0-0" className={pf('m-right--x-small m-left--xx-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Allied Technologies</a></th>
+      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0-0" className={pf('m-right--x-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Allied Technologies</a></th>
       <td data-label="employees"><a href="#" className={pf('truncate')}>390</a></td>
       <td data-label="phone-number"><span className={pf('truncate')}>837-555-1212</span></td>
       <td data-label="account-owner"><span className={pf('truncate')}>Jane Doe</span></td>
@@ -147,7 +147,7 @@ export default (
       </td>
     </tr>
     <tr className={pf('hint-parent')} id="tree0-node2-0-0-0-0" role="row" aria-level="6" aria-expanded="true">
-      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0-0-0" className={pf('m-right--x-small m-left--xx-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Allied Technologies (UV)</a></th>
+      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0-0-0" className={pf('m-right--x-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Allied Technologies (UV)</a></th>
       <td data-label="employees"><a href="#" className={pf('truncate')}>270</a></td>
       <td data-label="phone-number"><span className={pf('truncate')}>837-555-1212</span></td>
       <td data-label="account-owner"><span className={pf('truncate')}>John Doe</span></td>
@@ -162,7 +162,7 @@ export default (
       </td>
     </tr>
     <tr className={pf('hint-parent')} id="tree0-node2-0-0-0-0-0" role="row" aria-level="7" aria-expanded="true">
-      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0-0-0-0" className={pf('m-right--x-small m-left--xx-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>AB Partners</a></th>
+      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0-0-0-0" className={pf('m-right--x-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>AB Partners</a></th>
       <td data-label="employees"><a href="#" className={pf('truncate')}>260</a></td>
       <td data-label="phone-number"><span className={pf('truncate')}>837-555-1212</span></td>
       <td data-label="account-owner"><span className={pf('truncate')}>John Doe</span></td>
@@ -177,7 +177,7 @@ export default (
       </td>
     </tr>
     <tr className={pf('hint-parent')} id="tree0-node2-0-0-0-0-0-0" role="row" aria-level="8" aria-expanded="true">
-      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0-0-0-0-0" className={pf('m-right--x-small m-left--xx-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Styles and Sounds Inc</a></th>
+      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0-0-0-0-0" className={pf('m-right--x-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Styles and Sounds Inc</a></th>
       <td data-label="employees"><a href="#" className={pf('truncate')}>187</a></td>
       <td data-label="phone-number"><span className={pf('truncate')}>837-555-1212</span></td>
       <td data-label="account-owner"><span className={pf('truncate')}>Jane Doe</span></td>
@@ -192,7 +192,7 @@ export default (
       </td>
     </tr>
     <tr className={pf('hint-parent')} id="tree0-node2-0-0-0-0-0-0-0" role="row" aria-level="9" aria-expanded="true">
-      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0-0-0-0-0-0" className={pf('m-right--x-small m-left--xx-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Advanced Interconnections</a></th>
+      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0-0-0-0-0-0" className={pf('m-right--x-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Advanced Interconnections</a></th>
       <td data-label="employees"><a href="#" className={pf('truncate')}>150</a></td>
       <td data-label="phone-number"><span className={pf('truncate')}>837-555-1212</span></td>
       <td data-label="account-owner"><span className={pf('truncate')}>John Doe</span></td>
@@ -207,7 +207,7 @@ export default (
       </td>
     </tr>
     <tr className={pf('hint-parent')} id="tree0-node2-0-0-0-0-0-0-0-0" role="row" aria-level="10" aria-expanded="true">
-      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0-0-0-0-0-0-0" className={pf('m-right--x-small m-left--xx-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Net Inc</a></th>
+      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node2-0-0-0-0-0-0-0-0" className={pf('m-right--x-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Net Inc</a></th>
       <td data-label="employees"><a href="#" className={pf('truncate')}>120</a></td>
       <td data-label="phone-number"><span className={pf('truncate')}>837-555-1212</span></td>
       <td data-label="account-owner"><span className={pf('truncate')}>John Doe</span></td>
@@ -237,7 +237,7 @@ export default (
       </td>
     </tr>
     <tr className={pf('hint-parent')} id="tree0-node3" role="row" aria-level="2" aria-expanded="true">
-      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node3" className={pf('m-right--x-small m-left--xx-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Vand Enterprises</a></th>
+      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node3" className={pf('m-right--x-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Vand Enterprises</a></th>
       <td data-label="employees"><a href="#" className={pf('truncate')}>6,000</a></td>
       <td data-label="phone-number"><span className={pf('truncate')}>837-555-1212</span></td>
       <td data-label="account-owner"><span className={pf('truncate')}>John Doe</span></td>
@@ -267,7 +267,7 @@ export default (
       </td>
     </tr>
     <tr className={pf('hint-parent')} id="tree0-node4" role="row" aria-level="2" aria-expanded="true">
-      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node4" className={pf('m-right--x-small m-left--xx-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Tech Labs</a></th>
+      <th data-label="account-name" scope="row" className={pf('tree__item')}><ButtonIcon aria-controls="tree0-node4" className={pf('m-right--x-small')} flavor="icon-bare" iconFlavor="small" sprite="utility" symbol="chevronright" assistiveText="Toggle" /><a href="#" className={pf('truncate')}>Tech Labs</a></th>
       <td data-label="employees"><a href="#" className={pf('truncate')}>1,856</a></td>
       <td data-label="phone-number"><span className={pf('truncate')}>837-555-1212</span></td>
       <td data-label="account-owner"><span className={pf('truncate')}>John Doe</span></td>

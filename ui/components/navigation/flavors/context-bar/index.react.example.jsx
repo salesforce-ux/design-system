5f7e2aa076192ecf90bba2bc290aebeaf5516c76
Kaelig <kaelig@deloumeau.fr>
2016-03-17 16:42:21 -0700

    Add a third zone, improve accessibility and add a new link variation

--- ui/components/navigation/flavors/context-bar/index.react.example.jsx
+++ ui/components/navigation/flavors/context-bar/index.react.example.jsx
@@ -14,54 +14,86 @@ import SvgIcon from 'app_modules/ui/svg-icon';
 import Menu from 'ui/components/menus/index.react';
 import { prefix as pf } from 'app_modules/ui/util/component';
 
-export default (
-  <div className="demo-only" style={{height: '16rem'}}>
-    <div className={pf('context-bar grid')}>
-      <div className={pf('context-bar__shadow')}></div>
-      <div className={pf('context-bar__primary context-bar-action grid grid--vertical-align-stretch')}>
-        <a href="#void" className={pf('context-bar-action__label grid grid--vertical-align-center text-link--reset p-horizontal--large text-heading--small')}>
+
+//////////////////////////////////////////////
+// Partial(s)
+//////////////////////////////////////////////
+
+//////////////////////////////////////////////
+// State Constructor(s)
+//////////////////////////////////////////////
+
+let Default = props =>
+<div className="demo-only" style={{height: '16rem'}}>
+  <div className={pf('context-bar grid')}>
+    <div className={pf('context-bar__shadow')}></div>
+    <div className={pf('context-bar__primary grid')}>
+      <a role="button" href="#void" aria-haspopup="true" className={pf('context-bar__app-switcher button button--icon-bare grid grid--vertical-align-center p-left--large p-right--x-small')}>
+        <SvgIcon className={pf('button__icon button__icon--large')} sprite="utility" symbol="apps" />
+      </a>
+      <div className={pf('context-bar-action grid grid--vertical-align-stretch')}>
+        <a href="#void" className={pf('context-bar-action__text-label grid grid--vertical-align-center p-left--x-small p-right--large text-heading--small')}>
           Title
         </a>
       </div>
-      <nav className={pf('context-bar__secondary grid')} role="navigation">
-        <div className={pf('context-bar__vertical-divider')}></div>
-        <ul className={pf('grid grid--vertical-stretch')}>
-          <li className={pf('context-bar-action grid')}>
-            <a href="#void" className={pf('context-bar-action__label text-link--reset grid grid--vertical-align-center')}>Home</a>
-          </li>
-          <li className={pf('context-bar-action grid dropdown-trigger dropdown-trigger--click is-open')}>
-            <a href="#void" className={pf('context-bar-action__label context-bar-action__label--expand text-link--reset grid grid--vertical-align-center')}>Menu Item 1</a>
-            <button aria-haspopup="true" className={pf('context-bar-action__trigger button button--icon-bare')}>
-              <SvgIcon className={pf('button__icon button__icon--small context-bar-action__trigger-icon')} sprite="utility" symbol="down" />
-              <span className={pf('assistive-text')}>Assistive text for submenu</span>
-            </button>
-            <Menu className={pf('nubbin--top')}>
-              <Menu.List isSelectable={false}>
-                <Menu.Item>
-                  <SvgIcon className={pf('icon icon--x-small icon-text-default m-right--x-small')} sprite="utility" symbol="add" />
-                  Main action
-                </Menu.Item>
-              </Menu.List>
-              <hr className={pf('m-vertical--xx-small')} role="presentation" />
-              <div className={pf('dropdown__header')}>
-                <span className={pf('text-heading--label')}>Menu header</span>
-              </div>
-              <Menu.List isSelectable={false}>
-                <Menu.Item>Menu Item One</Menu.Item>
-                <Menu.Item>Menu Item Two</Menu.Item>
-                <Menu.Item>Menu Item Three</Menu.Item>
-              </Menu.List>
-            </Menu>
-          </li>
-          <li className={pf('context-bar-action grid')}>
-            <a href="#void" className={pf('context-bar-action__label context-bar-action__label--expand text-link--reset grid grid--vertical-align-center')}>Menu Item 2</a>
-            <button aria-haspopup="true" className={pf('context-bar-action__trigger button button--icon-bare')}>
-              <SvgIcon className={pf('button__icon button__icon--small context-bar-action__trigger-icon')} sprite="utility" symbol="down" />
-              <span className={pf('assistive-text')}>Assistive text for submenu</span>
-            </button>
-          </li>
-        </ul>
-      </nav>
+    </div>
+    <nav className={pf('context-bar__secondary grid')} role="navigation">
+      <div className={pf('context-bar__vertical-divider')}></div>
+      <ul className={pf('grid grid--vertical-stretch')} role="menubar">
+        <li role="presentation" className={pf('context-bar-action grid')}>
+          <a role="menuitem" aria-selected="true" href="#void" className={pf('context-bar-action__label is-selected text-link--reset grid grid--vertical-align-center')}>Home</a>
+        </li>
+        <li role="presentation" className={pf('context-bar-action grid dropdown-trigger dropdown-trigger--click is-open')}>
+          <a role="menuitem" aria-selected="false" href="#void" className={pf('context-bar-action__label context-bar-action__label--expand text-link--reset grid grid--vertical-align-center')}>Menu Item 1</a>
+          <button aria-haspopup="true" className={pf('context-bar-action__trigger button button--icon-bare')} id="context-bar-action-trigger-0">
+            <SvgIcon className={pf('button__icon button__icon--small context-bar-action__trigger-icon')} sprite="utility" symbol="down" />
+            <span className={pf('assistive-text')}>Assistive text for submenu</span>
+          </button>
+          <Menu aria-labelledby="context-bar-action-trigger-0" className={pf('nubbin--top')}>
+            <Menu.List isSelectable={false}>
+              <Menu.Item>
+                <SvgIcon className={pf('icon icon--x-small icon-text-default m-right--x-small')} sprite="utility" symbol="add" />
+                Main action
+              </Menu.Item>
+            </Menu.List>
+            <hr className={pf('m-vertical--xx-small')} role="presentation" />
+            <div className={pf('dropdown__header')}>
+              <span className={pf('text-heading--label')}>Menu header</span>
+            </div>
+            <Menu.List isSelectable={false}>
+              <Menu.Item>Menu Item One</Menu.Item>
+              <Menu.Item>Menu Item Two</Menu.Item>
+              <Menu.Item>Menu Item Three</Menu.Item>
+            </Menu.List>
+          </Menu>
+        </li>
+        <li role="presentation" className={pf('context-bar-action grid')}>
+          <a role="menuitem" aria-selected="false" href="#void" className={pf('context-bar-action__label context-bar-action__label--expand text-link--reset grid grid--vertical-align-center')}>Menu Item 2</a>
+          <button aria-haspopup="true" className={pf('context-bar-action__trigger button button--icon-bare')}>
+            <SvgIcon className={pf('button__icon button__icon--small context-bar-action__trigger-icon')} sprite="utility" symbol="down" />
+            <span className={pf('assistive-text')}>Assistive text for submenu</span>
+          </button>
+        </li>
+      </ul>
+    </nav>
+    <div className={pf('context-bar__tertiary grid col--bump-left')}>
+      <ul className={pf('grid grid--vertical-stretch')} role="menubar">
+        <li role="presentation" className={pf('context-bar-action grid')}>
+          <a role="menuitem" aria-selected="false" className={pf('context-bar-action__label text-link--reset grid grid--vertical-align-center')} href="#void">Actions</a>
+        </li>
+      </ul>
     </div>
   </div>
-);
+</div>;
+
+//////////////////////////////////////////////
+// Export
+//////////////////////////////////////////////
+
+export let states = [
+  {
+    id: 'default',
+    label: 'The kitchen sink',
+    element: <Default />
+  }
+];

4fb0cc851fc6f763ab289bb8192ab199e8d5a470
Brandon Ferrua <bferrua@gmail.com>
2016-04-25 15:27:28 -0700

    apply requested fixes in documentation

--- ui/components/global-navigation/index.docs.jsx
+++ ui/components/global-navigation/index.docs.jsx
@@ -28,13 +28,16 @@ export default (
       <img src="/assets/images/global-navigation/documentation/building-nav.svg" alt="" />
     </figure>
     <p>The context bar is composed of a wrapper and 3 regions.</p>
-    <ol>
-      <li><strong>Primary:</strong> App Switcher + App Name + Object Switcher</li>
-      <li><strong>Secondary:</strong> Contextual Navigation</li>
-      <li><strong>Tertiary:</strong> Contextual Actions</li>
-    </ol>
-    <p>The DOM should follow the order listed above where the Contextual Navigation, either hyperlinks or tabs, should be the last element. This will allow for proper keyboard interaction set by the 508 compliance standards.</p>
-    <p>Each region inside the wrapper of <CodeClass className="context-bar" />, will be described using ordering terms such as <strong>"primary"</strong> <strong>"secondary"</strong> <strong>"tertiary"</strong>. This will describe the DOM ordering, not the visual ordering since we want the navigation last for keyboard interactions but visually displayed as adjacent to the app switcher.</p>
+    <dl>
+      <dt><strong>Primary:</strong></dt>
+      <dd>App Switcher + App Name + Object Switcher</dd>
+      <dt><strong>Secondary:</strong></dt>
+      <dd>Contextual Navigation</dd>
+      <dt><strong>Tertiary:</strong></dt>
+      <dd>Contextual Actions</dd>
+    </dl>
+    <p>The markup should follow the order listed above where the Contextual Navigation, either hyperlinks or tabs, should be the last element. This will allow for proper keyboard interaction set by the 508 compliance standards.</p>
+    <p>Each region inside the wrapper of <CodeClass className="context-bar" />, will be described using ordering terms such as <strong>&ldquo;primary&rdquo;</strong>, <strong>&ldquo;secondary&rdquo;</strong>, <strong>&ldquo;tertiary&rdquo;</strong>. This will describe the markup ordering, not the visual ordering since we want the navigation last for keyboard interactions but visually displayed as adjacent to the app switcher.</p>
 
     <h2 className={pf('site-text-heading--small')}>1.1. Items on the Context Bar</h2>
     <figure className={pf('site-figure')}>
@@ -56,14 +59,14 @@ export default (
     </ol>
     <p>These layouts are built upon 2 reusable classes. The text hyperlink action area would get the class <CodeClass className="context-bar__label-action" /> and the icon based action area would get the class <CodeClass className="context-bar__icon-action" />.</p>
     <p>The <CodeClass className="context-bar__icon-action" /> class handles the alignment and coloring of the icons based on interactions such as <code>:hover</code>, <code>:focus</code> and <code>:active</code>. This is a required if you have an actionable icon area adjacent to an actionable text area.</p>
-    <p>The DOM structure should have the button icon(s) preceding the text label. The icons can be visually reordered by using the ordering utility classes such as <code>.slds-order--[#]</code>.</p>
+    <p>The markup structure should have the button icon(s) preceding the text label. The icons can be visually reordered by using the ordering utility classes such as <code>.slds-order--[#]</code>.</p>
 
     <h2 className={pf('site-text-heading--small')}>1.3. Building the Icons in the Context Bar</h2>
     <figure className={pf('site-figure')}>
       <img src="/assets/images/global-navigation/documentation/icons.svg" alt="" />
     </figure>
     <p>The icons in the context bar either serve as a visual affordance that indicates an item has a menu with additional actions or the icon itself is actionable.</p>
-    <p>The class used to indicate a button icon will sit adjacent to another actionable item is <CodeClass className="context-bar__icon-action" />.  If you two icons in an item, each icon should be contained in it's own <CodeClass className="context-bar__icon-action" /> element.</p>
+    <p>The class used to indicate a button icon will sit adjacent to another actionable item is <CodeClass className="context-bar__icon-action" />.  If you two icons in an item, each icon should be contained in it&rsquo;s own <CodeClass className="context-bar__icon-action" /> element.</p>
     <p>The look and feel of the button icon is dependant on the modifiers used from <a href="/components/buttons/#icon">Button Icon Variant Component</a> page.</p>
     <p>The <CodeClass className="context-bar__icon-action" /> styles apply a default left and right padding of 0.75rem. That padding can be removed or modified by using our spacing utilities. In example #1, the left padding has been removed by using the class <CodeClass className="p-left--none" />.</p>
     <p>If a context bar item has an icon then we want to provide visual indication that its active when a user hovers into its region. To make this happen, apply the class <CodeClass className="hint-parent" /> to the hover region, most cases would be <CodeClass className="context-bar__icon-action" /> element.</p>
@@ -76,7 +79,7 @@ export default (
     <ol>
       <li><CodeClass className="context-bar__dropdown-trigger" /> — Add a hoverable zone below the action so that the dropdown doesn't disappear when the mouse is between the action and the dropdown — this is specific to the context bar</li>
       <li><CodeClass className="dropdown-trigger" /> — Globally used utility that notifies the element there is a dropdown menu </li>
-      <li><code>.slds-dropdown-trigger--[interaction]</code> — Specify which type of interaction the menu should appear with "hover" or "click". The default pattern requires hover to show the menu besides the App Switcher Waffle icon.</li>
+      <li><code>.slds-dropdown-trigger--[interaction]</code> — Specify which type of interaction the menu should appear with &ldquo;hover&rdquo; or &ldquo;click&rdquo;. The default pattern requires hover to show the menu besides the App Switcher Waffle icon.</li>
     </ol>
     <p>If the navigation item has one hyperlink with a menu containing additional sub actions, then the hover area that invokes the dropdown menu is both the <CodeClass className="context-bar__label-action" /> and <CodeClass className="context-bar__icon-action" />. If the navigation item is demonstrated as a tab format, one hyperlink plus two additional actions, then the hover area that invokes the dropdown menu is only on the <CodeClass className="context-bar__icon-action" />.</p>
 
@@ -102,7 +105,8 @@ export default (
     <figure className={pf('site-figure')}>
       <img src="/assets/images/global-navigation/documentation/divider.svg" alt="" />
     </figure>
-    <p>Due to the platform-esc nature of the context bar, there is an available element utility to apply a divider between sections where needed. The <CodeClass className="context-bar__vertical-divider" /> class should be applied to an empty `div` and placed within the DOM of your context bar for a visual divider.</p>
+    <p>Due to the platform-esc nature of the context bar, there is an available element utility to apply a divider between sections where needed. The <CodeClass className="context-bar__vertical-divider" /> class should be applied to an empty <code>&lt;div&gt;</code> and placed within the markup of your context bar for a visual divider.</p>
+    <p>Alternatively, two classes are available to add border using css. Adding the class <CodeClass className="context-bar__item--divider-left" /> will add a left border to a context bar item while <CodeClass className="context-bar__item--divider-right" /> will add a right border.</p>
 
     <h2 className={pf('site-text-heading--small')}>2.0. Theming</h2>
     <p>Coming Soon</p>

4def120103f7568df74ddf84391d4f2c3b0fb96c
Brandon Ferrua <bferrua@gmail.com>
2016-05-02 14:25:07 -0700

    remove comments

--- ui/components/global-header/flavors/base/index.react.example.jsx
+++ ui/components/global-header/flavors/base/index.react.example.jsx
@@ -11,7 +11,6 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 import React from 'react';
 import SvgIcon from 'app_modules/ui/svg-icon';
-// import { ButtonIcon } from 'ui/components/buttons/flavors/icon/index.react.example';
 import { Menu, MenuList, MenuItem } from 'ui/components/menus/flavors/dropdown/index.react.example';
 import className from 'classnames';
 import { prefix as pf } from 'app_modules/ui/util/component';

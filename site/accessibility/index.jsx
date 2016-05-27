f95c797fe3167d4681b81479cd023ba00df35e0c
Richard Boardman <rboardman@salesforce.com>
2016-05-27 16:25:51 -0700

    Tidy typos

--- site/accessibility/index.jsx
+++ site/accessibility/index.jsx
@@ -17,53 +17,43 @@ import globals from 'app_modules/global';
 export default (
   <PageBody anchorTitle="Accessibility" contentClassName={pf('site-design site-design-layout')}>
     <div className={pf('container--large')}>
-      <p className="site-text-introduction">The Lightning Design System provides accessible markup which will serve as a foundation for your application development. In order to make sure you build accessible components however, you will need to follow the accessibility guidance for our interactive components. This includes keyboard behavior as well as the management of ARIA roles and properties.</p>
+      <p className="site-text-introduction">The Lightning Design System provides accessible markup which will serve as a foundation for your application development. In order to make sure you build accessible components, however, you will need to follow the accessibility guidance for our interactive components. This includes keyboard behavior as well as the management of ARIA roles and properties.</p>
 
-      {/* What is Accessibility? */}
       <h2 className="site-text-heading--large">What is Accessibility?</h2>
-      <p>Web accessibility ensures that people with disabilities can perceive, understand, navigate, interact with, and contribute to the applications you create.  This means that pages are <a href="https://www.w3.org/TR/WCAG20/">Perceivable, Operable, Understandable and Robust</a>. This includes providing keyboard interaction alternatives for all mouse based actions, properly identifying all form fields and buttons, providing text based alternatives for all images, videos, icons and SVGs, as well as building components that properly convey their identity, operation model, and state to assistive technologies.</p>
+      <p>Web accessibility ensures that people with disabilities can perceive, understand, navigate, interact with, and contribute to the applications you create. This means that pages are <a href="https://www.w3.org/TR/WCAG20/">Perceivable, Operable, Understandable and Robust</a>. This includes providing keyboard interaction alternatives for all mouse-based actions, properly identifying all form fields and buttons, providing text based alternatives for all images, videos, icons and SVGs, as well as building components that properly convey their identity, operation model, and state to assistive technologies.</p>
       <p>The Design System enables accessible development by providing a set of semantically correct components, each with appropriate <a href="https://www.w3.org/TR/wai-aria/">ARIA</a> markup so they can be identified correctly to users of assistive technologies.</p>
 
-
-      {/* Standards Compliant Markup */}
       <h2 className="site-text-heading--large">Standards Compliant Markup</h2>
       <p>The semantic markup and use of ARIA roles in our components are based on W3C standards and industry best practices. This markup is the perfect starting point for building accessible components.</p>
 
-      {/* Keyboard Navigation */}
       <h2 className="site-text-heading--large">Keyboard Navigation</h2>
-      <p>While we do not provide the JavaScript that is necessary to make our components interactive, we offer advice on how to use scripting languages to create keyboard accessible components.</p>
+      <p>While we do not provide the JavaScript that is necessary to make our components interactive, we offer advice on how to use scripting languages to create keyboard-accessible components.</p>
 
-      {/* Appropriate use of colour */}
-      <h2 className="site-text-heading--large">Appropriate use of colour</h2>
+      <h2 className="site-text-heading--large">Appropriate use of color</h2>
       <p>Our components follow the two main rules of accessibility as it relates to color:</p>
       <ul>
         <li>We never use color as the only means of providing information or requesting an action.</li>
-        <li>The combinations of text and their background colors do not fall below the <a href="https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-contrast.html">WCAG recommended threshold</a> ratio of 4.5:1 for standard or small text and 3:1 for larger text..</li>
+        <li>The combinations of text and their background colors do not fall below the <a href="https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-contrast.html">WCAG recommended threshold</a> ratio of 4.5:1 for standard or small text and 3:1 for larger text.</li>
       </ul>
 
-      {/* Accessible Forms */}
       <h2 className="site-text-heading--large">Accessible Forms</h2>
       <p>Our forms offer proper use of <code>&lt;fieldset&gt;</code> and <code>&lt;legend&gt;</code> tags as well as appropriate labeling for input controls. Our radio button and checkbox controls provide a balanced solution that offers accessibility as well as design flexibility.</p>
 
-      {/* Images and icons */}
       <h2 className="site-text-heading--large">Images and icons</h2>
-      <p>We provide a means of offering text based alternatives for all images, icons and SVGs.</p>
+      <p>We provide a means of offering text-based alternatives for all images, icons and SVGs.</p>
 
-      {/* Component Identity*/}
       <h2 className="site-text-heading--large">Component Identity</h2>
       <p>Our interactive components are created in accordance with the latest <a href="http://w3c.github.io/aria/practices/aria-practices.html">ARIA Authoring Practices</a>, so that they are understandable by screen reader users. Our Accessibility guidance and component state variants, also describe how those ARIA attributes should change as a component is interacted with, to best describe the components state to assistive technology.</p>
 
-      {/* Validating your applications */}
       <h2 className="site-text-heading--large">Validating your applications</h2>
       <p>The Design System is only the foundation for accessible application development. We recommend that you review the accessibility of your applications before release and ensure that it meets the <a href="https://www.w3.org/TR/WCAG20/#conformance">WCAG Standard at the AA Level</a>.</p>
 
-      {/* Resources */}
       <h2 className="site-text-heading--large">Resources</h2>
       <ul>
         <li><a href="https://www.w3.org/WAI/">W3C Web Accessibility Initiative</a></li>
         <li><a href="http://w3c.github.io/aria/practices/aria-practices.html">WAI-ARIA Authoring Practices</a></li>
         <li><a href="https://medium.com/salesforce-ux/7-things-every-designer-needs-to-know-about-accessibility-64f105f0881b#.o8n02j9rr">7 Things Every Designer Needs to Know about Accessibility</a></li>
-        <li><a href="https://medium.com/salesforce-ux/accessible-interface-design-d80e95cbb2c1#.i5tl6ffv3">Accessible Interface Design: on designing with accessible colour contrast ratios</a></li>
+        <li><a href="https://medium.com/salesforce-ux/accessible-interface-design-d80e95cbb2c1#.i5tl6ffv3">Accessible Interface Design: on designing with accessible color contrast ratios</a></li>
         <li><a href="http://webaim.org/">WebAIM</a></li>
         <li><a href="http://a11yproject.com/">The A11Y Project</a></li>
       </ul>

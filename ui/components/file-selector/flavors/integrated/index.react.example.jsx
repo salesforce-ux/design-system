4101ae1861a7a3455c866c77754c40fdb8f387e4
Brandon Ferrua <bferrua@gmail.com>
2016-06-29 16:07:22 -0700

    Scope elements of the integrated file selector to deal with nested file selectors

--- ui/components/file-selector/flavors/integrated/index.react.example.jsx
+++ ui/components/file-selector/flavors/integrated/index.react.example.jsx
@@ -16,14 +16,14 @@ import { FileSelector } from 'ui/components/file-selector/flavors/files/index.re
 import { prefix as pf } from 'app_modules/ui/util/component';
 
 let IntegratedFileSelector = props =>
-  <div className={className(pf('file-selector'), props.className)}>
-    <div className={className(pf('file-selector__dropzone'), props.drag ? pf('has-drag') : null, props.draggover ? pf('has-drag-over') : null)} aria-hidden="true">
+  <div className={className(pf('file-selector file-selector--integrated'), props.className)}>
+    <div className={className(pf('file-selector__dropzone file-selector__dropzone--integrated'), props.drag ? pf('has-drag') : null, props.draggover ? pf('has-drag-over') : null)} aria-hidden="true">
       <input className={pf('file-selector__input assistive-text')} accept="image/png" type="file" id="file-upload-input-01" disabled={props.draggoverError} tabIndex="-1" />
-      <label className={pf('file-selector__body')} htmlFor="file-upload-input-01">
+      <label className={pf('file-selector__body file-selector__body--integrated')} htmlFor="file-upload-input-01">
         { props.draggoverError ?
           <SvgIcon className={pf('file-selector__body-icon icon icon-text-default')} sprite="utility" symbol="ban" />
         : <SvgIcon className={pf('file-selector__body-icon icon icon-text-default')} sprite="utility" symbol="upload" /> }
-        <span className={pf('file-selector__text text-heading--medium text-align--center')}>
+        <span className={pf('file-selector__text file-selector__text--integrated text-heading--medium text-align--center')}>
           { props.draggoverError ? 'Too many files selected. Attach up to 1 file.' : 'Drop Files' }</span>
       </label>
     </div>

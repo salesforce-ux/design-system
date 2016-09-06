4df1400ce47babb4517b25afab8ecf8d31ad898f
Brandon Ferrua <bferrua@gmail.com>
2016-09-06 11:48:02 -0700

    Remove comments

--- ui/utilities/grid/flavors/automatic-sizing/index.react.example.jsx
+++ ui/utilities/grid/flavors/automatic-sizing/index.react.example.jsx
@@ -11,35 +11,6 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 import React from 'react';
 import { prefix as pf } from 'app_modules/ui/util/component';
-//
-//
-// export const preview = (
-//   <div className="demo-only demo-container">
-//     <div className={pf('grid')}>
-//       <div className={pf('col')}><div className={pf('box box--small theme--shade text-align--center')}>col</div></div>
-//       <div className={pf('col')}><div className={pf('box box--small theme--shade text-align--center')}>col</div></div>
-//     </div>
-//
-//     <div className={pf('grid grid--pull-padded')} style={{marginTop: '2rem'}}>
-//       <div className={pf('col--padded')}><div className={pf('box box--small theme--shade text-align--center')}>col--padded</div></div>
-//       <div className={pf('col--padded')}><div className={pf('box box--small theme--shade text-align--center')}>col--padded</div></div>
-//     </div>
-//   </div>
-// );
-//
-// export const code = (
-//   <div className="demo-only demo-container">
-//     <div className={pf('grid')}>
-//       <div className={pf('col')}>col</div>
-//       <div className={pf('col')}>col</div>
-//     </div>
-//
-//     <div className={pf('grid grid--pull-padded')}>
-//       <div className={pf('col--padded')}>col--padded</div>
-//       <div className={pf('col--padded')}>col--padded</div>
-//     </div>
-//   </div>
-// );
 
 export let states = [
   {

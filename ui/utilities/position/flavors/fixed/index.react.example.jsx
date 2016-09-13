101f8d4125119402264fd0ba779dffabf1437010
Stephanie Rewis <srewis@salesforce.com>
2016-09-13 13:25:19 -0700

    Missed double classes slds

--- ui/utilities/position/flavors/fixed/index.react.example.jsx
+++ ui/utilities/position/flavors/fixed/index.react.example.jsx
@@ -13,7 +13,7 @@ import React from 'react';
 
 
 export default (
-  <div className="demo-only grid" style={{ height: '150px', width: '300px', padding: '2rem' }}>
+  <div className="demo-only slds-grid" style={{ height: '150px', width: '300px', padding: '2rem' }}>
     <div className="slds-is-fixed">
       <div style={{ position: 'absolute', top: '1rem', left: '1rem', border: '1px solid red', background: '#f4f6f9' }}>An element with fixed positioning is positioned relative to the viewport. If no other positioning values are given (<code>top, right, bottom, left</code>) it will start its positioning relative to where it is in the flow of the page.</div>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>

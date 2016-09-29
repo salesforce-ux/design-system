bc9d3a3c68611d44616fa797b3b2bf379f370a69
Heroku <slds-ops@salesforce.com>
2016-09-29 18:35:44 +0000

    "Initial commit"

---
+++ design-tokens/dist/force-mq-commons.common.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mqSmall: "only screen and (max-width: 47.9375em)",
+  mqMedium: "only screen and (min-width: 48em)",
+  mqMediumLandscape: "only screen and (min-width: 48em) and (min-aspect-ratio: 4/3)",
+  mqLarge: "only screen and (min-width: 64.0625em)",
+  mqHighRes: "only screen and (-webkit-min-device-pixel-ratio: 2), screen and (min-device-pixel-ratio: 2),screen and (min-resolution: 192dpi),screen and (min-resolution: 2dppx)",
+  mqSingleColumnRecordLayout: "(max-width: 599px)"
+};
---
+++ design-tokens/dist/s1-base-large.common.js
@@ -0,0 +1,14 @@
+module.exports = {
+  cardColorBackground: "rgb(244, 246, 249)",
+  widthStageLeftCollapsed: "4rem",
+  widthStageLeftExpanded: "15rem",
+  heightHeader: "3.125rem",
+  squareIconMedium: "2rem",
+  fontSizeSmall: "0.75rem",
+  fontSizeMedium: "0.8125rem",
+  fontSizeMediumA: "0.875rem",
+  fontSizeLarge: "1rem",
+  fontSizeXLarge: "1.25rem",
+  fontSizeXxLarge: "1.75rem",
+  lineHeightText: "1.5"
+};

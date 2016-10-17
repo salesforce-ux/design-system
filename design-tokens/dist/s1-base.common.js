0c96e8b495270864c9f2a942ce212ae28d84ab23
Heroku <slds-ops@salesforce.com>
2016-10-17 20:48:11 +0000

    "Initial commit"

---
+++ design-tokens/dist/s1-base.common.js
@@ -0,0 +1,22 @@
+module.exports = {
+  widthStageLeft: "88%",
+  heightFooter: "3.75rem",
+  heightHeader: "2.75rem",
+  heightInput: "1.875rem",
+  heightOverflowStart: "144px",
+  maxHeightActionOverflowMenu: "628px",
+  maxHeightCardMedia: "144px",
+  maxHeightHeaderBrand: "32px",
+  maxWidthActionBar: "600px",
+  maxWidthActionBarItem: "120px",
+  maxWidthActionBarWithLabels: "320px",
+  maxWidthActionOverflowMenu: "512px",
+  maxWidthHeaderBrand: "156px",
+  squareRecordAnchorPic: "3rem",
+  squareTappable: "2.75rem",
+  flexMasterPanel: "7",
+  squareTooltip: "1.25rem",
+  squareTooltipNubbin: "1rem",
+  flexDetailPanel: "3",
+  durationSlide: "0.25s"
+};

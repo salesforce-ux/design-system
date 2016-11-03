d4d3e0f784dfa6326e3954de5edf1b04d6dde7c3
Isha Kasliwal <ikasliwal@salesforce.com>
2016-11-03 11:07:13 -0600

    checkpoint

--- ui/components/menus/flavors/dynamic-list/index.react.example.jsx
+++ ui/components/menus/flavors/dynamic-list/index.react.example.jsx
@@ -12,11 +12,11 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 import React from 'react';
 import { ButtonIcon } from 'ui/components/button-icons/flavors/base/index.react.example';
 import { Trigger } from 'ui/components/menus/flavors/dropdown/index.react.example';
-import { Menu } from 'ui/components/menus/flavors/dropdown/index.react.example';
-import { MenuList } from 'ui/components/menus/flavors/dropdown/index.react.example';
-import { MenuItem } from 'ui/components/menus/flavors/dropdown/index.react.example';
+import { Menu, MenuList, MenuItem } from 'ui/components/menus/flavors/dropdown/index.react.example';
 import { Lookup } from 'ui/components/lookups/flavors/single/index.react.example.jsx';
+import { Listbox, ListboxList, ListboxItem } from 'ui/components/picklist/flavors/base/index.react.example.jsx';
 import SvgIcon from 'app_modules/ui/svg-icon';
+import _ from 'lodash';
 
 ///////////////////////////////////////////
 // Partial(s)
@@ -32,6 +32,33 @@ let LookupTest = props =>
     </footer>
   </div>;
 
+let MediaItem = props =>
+  <span className="slds-lookup__item-action slds-media slds-media--center slds-p-around--none">
+    <SvgIcon className="slds-icon slds-icon-standard-account slds-icon--small slds-media__figure" sprite="standard" symbol="account" />
+    <div className="slds-media__body">
+      <div className="slds-lookup__result-text">Salesforce.com, Inc.</div>
+      <span className="slds-lookup__result-meta slds-text-body--small">Account &bull; San Francisco</span>
+    </div>
+  </span>;
+
+let FavoriteItem = props =>
+  <div className="slds-grid">
+    <div className="slds-text-align--center">
+      <SvgIcon className="slds-button__icon--stateful slds-button__icon--left" sprite="utility" symbol="add" />
+    </div>
+    <div className="slds-text-align--center">
+      Favorite this page
+    </div>
+  </div>;
+
+let EditItem = props =>
+  <div className="slds-grid">
+    <div className="slds-text-align--center">
+      <SvgIcon className="slds-button__icon--stateful slds-button__icon--left" sprite="utility" symbol="edit" />
+    </div>
+    <div className="slds-text-align--center">Edit Favorites</div>
+  </div>;
+
 //////////////////////////////////////////////
 // Export
 //////////////////////////////////////////////
@@ -41,41 +68,100 @@ export let states = [
     id: 'dynamic-list-0-items',
     label: '0 items',
     element:
-      <div className="slds-popover slds-nubbin--left" role="dialog">
-        <div className="slds-popover__body">
-          <MenuList>
-            <li className="slds-dropdown__header" role="separator">
-              <span className="slds-text-title--caps">Favorites</span>
-            </li>
-            <li className="slds-p-around--small">
-              <p>You can favorite any page that you care about, and it will be accessible whenever you need it.</p>
-            </li>
-          </MenuList>
+      <div className="slds-popover slds-nubbin--top-right" role="dialog">
+        <div className="slds-popover__body slds-p-around--none">
+          <ListboxList header>
+            <ListboxItem headerText="Favorites" className="slds-text-title--caps slds-p-top--x-small">Favorites</ListboxItem>
+            <ListboxItem>You can favorite any page!</ListboxItem>
+          </ListboxList>
         </div>
-        <footer className="slds-popover__footer slds-shrink-none">
+        <footer className="slds-popover__footer slds-p-horizontal--none slds-shrink-none">
+          <ListboxList>
+            <ListboxItem>
+              <FavoriteItem />
+            </ListboxItem>
+            <ListboxItem>
+              <EditItem />
+            </ListboxItem>
+          </ListboxList>
+        </footer>
+      </div>
+  },
+  {
+    id: 'dynamic-list-1-item',
+    label: '1 item',
+    element:
+      <div className="slds-popover slds-nubbin--top-right" role="dialog">
+        <div className="slds-popover__body slds-p-around--none">
           <MenuList>
+            <MenuItem className="slds-dropdown__header slds-text-title--caps slds-p-bottom--xxx-small slds-p-horizontal--none" role="separator">
+              Favorites
+            </MenuItem>
             <MenuItem>
-              <div className="slds-grid">
-                <div>
-                  <div className="slds-text-align--center">
-                    <SvgIcon className="slds-button__icon--stateful slds-button__icon--left" sprite="utility" symbol="add" />
-                  </div>
-                </div>
-                <div>
-                  <div className="slds-text-align--center">
-                    Favorite this page
-                  </div>
-                </div>
-              </div>
+              <MediaItem />
+            </MenuItem>
+            <li className="slds-has-divider--top" role="separator"></li>
+            <MenuItem>
+              <FavoriteItem />
+            </MenuItem>
+            <MenuItem>
+              <EditItem />
             </MenuItem>
           </MenuList>
-        </footer>
+        </div>
+      </div>
+  },
+  {
+    id: 'dynamic-list-less-than-10-items',
+    label: '< 10 items',
+    element:
+      <div className="slds-popover slds-nubbin--top-right" role="dialog">
+        <div className="slds-popover__body slds-p-around--none">
+          <MenuList>
+            <MenuItem className="slds-dropdown__header slds-text-title--caps slds-p-bottom--xxx-small slds-p-horizontal--none" role="separator">
+              Favorites
+            </MenuItem>
+            { _.times(7, i =>
+              <MenuItem>
+                <MediaItem />
+              </MenuItem>
+            )}
+            <li className="slds-has-divider--top" role="separator"></li>
+            <MenuItem>
+              <FavoriteItem />
+            </MenuItem>
+            <MenuItem>
+              <EditItem />
+            </MenuItem>
+          </MenuList>
+        </div>
       </div>
   },
   {
     id: 'dynamic-list-10-plus-items',
     label: '> 10 items',
     element:
-      <p>soon</p>
+      <div className="slds-popover slds-nubbin--top-right" role="dialog">
+        <div className="slds-popover__body slds-p-around--none">
+          <ListboxList header className="slds-dropdown--length-10">
+            <ListboxItem headerText="Favorites" className="slds-text-title--caps slds-p-top--x-small">Favorites</ListboxItem>
+            { _.times(11, i =>
+              <ListboxItem>
+                <MediaItem />
+              </ListboxItem>
+            )}
+          </ListboxList>
+        </div>
+        <footer className="slds-popover__footer slds-p-horizontal--none slds-shrink-none">
+          <ListboxList>
+            <ListboxItem>
+              <FavoriteItem />
+            </ListboxItem>
+            <ListboxItem>
+              <EditItem />
+            </ListboxItem>
+          </ListboxList>
+        </footer>
+      </div>
   }
 ];

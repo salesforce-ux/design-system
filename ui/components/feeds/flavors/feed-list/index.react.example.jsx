1c93e045e868b32d6726abf4f6e325f7f9b69053
Brandon Ferrua <bferrua@gmail.com>
2016-11-04 14:12:03 -0700

    Add top border when on mobile

--- ui/components/feeds/flavors/feed-list/index.react.example.jsx
+++ ui/components/feeds/flavors/feed-list/index.react.example.jsx
@@ -18,6 +18,7 @@ import { Comment, CommentHeader, CommentContent, CommentFooter } from 'ui/compon
 ///////////////////////////////////////////
 
 export default (
+  <div className="demo-only" style={{background: '#F4F6F9'}}>
   <div className="slds-feed">
     <ul className="slds-feed__list">
       <li className="slds-feed__item">
@@ -62,5 +63,6 @@ export default (
         </Post>
       </li>
     </ul>
+  </div>
   </div>
 );

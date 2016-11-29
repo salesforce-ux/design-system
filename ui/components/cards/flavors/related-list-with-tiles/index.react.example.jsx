62d8276c76bf9134346ee8fefd19b2570cd58e4e
Donielle Berg <donielle.berg@gmail.com>
2016-11-29 11:38:15 -0800

    a11y - make card markup more semantic

--- ui/components/cards/flavors/related-list-with-tiles/index.react.example.jsx
+++ ui/components/cards/flavors/related-list-with-tiles/index.react.example.jsx
@@ -29,17 +29,41 @@ const icon = (
 ///////////////////////////////////////////
 // Export
 ///////////////////////////////////////////
-
 export default (
   <Card>
-    <CardHeader symbol="contact" actions><span className="slds-text-heading--small">Contacts (3)</span></CardHeader>
+    <CardHeader actions symbol="contact">
+      <span className="slds-text-heading--small">Contacts (3)</span>
+    </CardHeader>
     <CardBody>
-      <div className="slds-card__body--inner slds-grid slds-wrap slds-grid--pull-padded">
-        <TileMedia title="Related Record Title 1" media={ icon } actions className="slds-card__tile slds-p-horizontal--small slds-size--1-of-1 slds-medium-size--1-of-3" />
-        <TileMedia title="Related Record Title 1" media={ icon } actions className="slds-card__tile slds-p-horizontal--small slds-size--1-of-1 slds-medium-size--1-of-3" />
-        <TileMedia title="Related Record Title 1" media={ icon } actions className="slds-card__tile slds-p-horizontal--small slds-size--1-of-1 slds-medium-size--1-of-3" />
-      </div>
+        <ul className="slds-card__body--inner slds-grid slds-wrap slds-grid--pull-padded">
+          <li className="slds-p-horizontal--small slds-size--1-of-1 slds-medium-size--1-of-3">
+            <TileMedia
+              actions
+              className="slds-card__tile"
+              media={ icon }
+              title="Related Record Title 1"
+            />
+          </li>
+          <li className="slds-p-horizontal--small slds-size--1-of-1 slds-medium-size--1-of-3">
+            <TileMedia
+              actions
+              className="slds-card__tile"
+              media={ icon }
+              title="Related Record Title 1"
+            />
+          </li>
+          <li className="slds-p-horizontal--small slds-size--1-of-1 slds-medium-size--1-of-3">
+            <TileMedia
+              actions
+              className="slds-card__tile"
+              media={ icon }
+              title="Related Record Title 1"
+            />
+          </li>
+        </ul>
     </CardBody>
-    <CardFooter><a href="javascript:void(0);">View All <span className="slds-assistive-text">entity type</span></a></CardFooter>
+    <CardFooter>
+      <a href="javascript:void(0);">View All <span className="slds-assistive-text">entity type</span></a>
+    </CardFooter>
   </Card>
 );

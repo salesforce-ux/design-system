d09936172ddff3f47fab476afd1d887140a4fb16
Kaelig <kaelig@deloumeau.fr>
2016-12-14 16:25:29 -0800

    Add a paused state to the Trend icon

--- ui/components/dynamic-icons/flavors/trend/index.react.example.jsx
+++ ui/components/dynamic-icons/flavors/trend/index.react.example.jsx
@@ -11,37 +11,72 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 import React from 'react';
 import _ from 'lodash';
+import classNames from 'classnames';
 
-let Trend = props =>
-  <span
-    {...props}
-    className="slds-icon-trend slds-is-animated"
-    title="Description of the icon when needed">
-    <svg viewBox="0 0 16 16" aria-hidden="true">
-      <path className="slds-icon-trend__arrow" d="M.75 8H11M8 4.5L11.5 8 8 11.5" />
-      <circle className="slds-icon-trend__circle" cy="8" cx="8" r="7.375" transform="rotate(-28 8 8) scale(-1 1) translate(-16 0)" />
-    </svg>
-    <span className="slds-assistive-text">Text alternative when needed</span>
-  </span>;
+let Trend = props => {
+  const { assistiveText, ...rest } = props;
 
-///////////////////////////////////////////
-// Export
-///////////////////////////////////////////
+  return (
+    <span
+      {...rest}
+      className={classNames('slds-icon-trend', props.className)}>
+      <svg viewBox="0 0 16 16" aria-hidden="true">
+        <path className="slds-icon-trend__arrow" d="M.75 8H11M8 4.5L11.5 8 8 11.5" />
+        <circle className="slds-icon-trend__circle" cy="8" cx="8" r="7.375" transform="rotate(-28 8 8) scale(-1 1) translate(-16 0)" />
+      </svg>
+      <span className="slds-assistive-text">{assistiveText}</span>
+    </span>
+  );
+};
+
+Trend.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  assistiveText: React.PropTypes.string.isRequired
+};
 
 export let states = [
   {
     id: 'down',
     label: 'Down',
-    element: <Trend data-slds-trend="down" />
+    element: <Trend
+      data-slds-trend="down"
+      className="slds-is-animated"
+      title="Description of the icon"
+      assistiveText="Text alternative" />
   },
   {
     id: 'neutral',
     label: 'Neutral',
-    element: <Trend data-slds-trend="neutral" />
+    element: <Trend
+      data-slds-trend="neutral"
+      className="slds-is-animated"
+      title="Description of the icon"
+      assistiveText="Text alternative" />
   },
   {
     id: 'up',
     label: 'Up',
-    element: <Trend data-slds-trend="up" />
+    element: <Trend
+      data-slds-trend="up"
+      className="slds-is-animated"
+      title="Description of the icon"
+      assistiveText="Text alternative" />
+  },
+  {
+    id: 'static',
+    label: 'Static (no animation)',
+    element: <Trend
+      data-slds-trend="up"
+      title="Description of the icon"
+      assistiveText="Text alternative" />
+  },
+  {
+    id: 'paused',
+    label: 'Paused',
+    element: <Trend
+      data-slds-trend="up"
+      className="slds-is-animated slds-is-paused"
+      title="Description of the icon"
+      assistiveText="Text alternative" />
   }
 ];

149c6979b78b09b3e0be833594736fceb392d2d8
Heroku <slds-ops@salesforce.com>
2017-01-10 21:06:55 +0000

    "Initial commit"

---
+++ design-tokens/dist/force-font-commons.common.js
@@ -0,0 +1,12 @@
+module.exports = {
+  salesforceSansRegularWoff2: "/slds/fonts/v2.3.0/SalesforceSans-Regular.woff2",
+  salesforceSansRegularWoff: "/slds/fonts/v2.3.0/SalesforceSans-Regular.woff",
+  salesforceSansLightWoff2: "/slds/fonts/v2.3.0/SalesforceSans-Light.woff2",
+  salesforceSansLightWoff: "/slds/fonts/v2.3.0/SalesforceSans-Light.woff",
+  salesforceSansBoldWoff2: "/slds/fonts/v2.3.0/SalesforceSans-Bold.woff2",
+  salesforceSansBoldWoff: "/slds/fonts/v2.3.0/SalesforceSans-Bold.woff",
+  salesforceSansLightItalicWoff2: "/slds/fonts/v2.3.0/SalesforceSans-LightItalic.woff2",
+  salesforceSansLightItalicWoff: "/slds/fonts/v2.3.0/SalesforceSans-LightItalic.woff",
+  salesforceSansItalicWoff2: "/slds/fonts/v2.3.0/SalesforceSans-Italic.woff2",
+  salesforceSansItalicWoff: "/slds/fonts/v2.3.0/SalesforceSans-Italic.woff"
+};

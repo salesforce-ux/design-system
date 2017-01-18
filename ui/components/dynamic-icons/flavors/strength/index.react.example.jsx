37be7ba7f24afa4714d03a555edb201dff87c148
Kaelig <kaelig@deloumeau.fr>
2017-01-18 15:34:05 -0800

    Improve 'roundness' of the circles

--- ui/components/dynamic-icons/flavors/strength/index.react.example.jsx
+++ ui/components/dynamic-icons/flavors/strength/index.react.example.jsx
@@ -18,9 +18,9 @@ let Strength = props =>
     title="Description of the icon when needed"
     className={classNames('slds-icon-strength', props.className)}>
     <svg viewBox="0 0 27 7" aria-hidden="true">
-      <circle r="3.1175" cx="3.5" cy="3.5" />
-      <circle r="3.1175" cx="13.5" cy="3.5" />
-      <circle r="3.1175" cx="23.5" cy="3.5" />
+      <circle r="3.025" cx="3.5" cy="3.5" />
+      <circle r="3.025" cx="13.5" cy="3.5" />
+      <circle r="3.025" cx="23.5" cy="3.5" />
     </svg>
     <span className="slds-assistive-text">Text alternative when needed</span>
   </span>;

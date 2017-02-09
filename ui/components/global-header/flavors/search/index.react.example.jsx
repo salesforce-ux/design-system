16396dea2a0e798369299f2336948b55d405c8ee
Si Taggart <me@simontaggart.com>
2017-02-08 17:13:29 -0800

    Global Header: Update to ARIA 1.1 combobox markup

--- ui/components/global-header/flavors/search/index.react.example.jsx
+++ ui/components/global-header/flavors/search/index.react.example.jsx
@@ -26,73 +26,7 @@ export let states = [
     label: 'Active',
     element:
       <div className="demo-only" style={{ height: '290px' }}>
-        <GlobalHeader autocomplete>
-          <div className="slds-form-element__control slds-input-has-icon slds-input-has-icon--left">
-            <SvgIcon className="slds-input__icon" sprite="utility" symbol="search" />
-            <input
-              id="global-search-01"
-              className="slds-input slds-lookup__search-input"
-              type="search"
-              placeholder="Search Salesforce"
-              aria-owns="global-search-list-01"
-              role="combobox"
-              aria-activedescendant=""
-              aria-expanded="true"
-              aria-autocomplete="list" />
-          </div>
-          <div className="slds-lookup__menu" role="listbox" id="global-search-list-01">
-            <ul className="slds-lookup__list" role="group" aria-label="Recent Items">
-              <li role="presentation">
-                <h3 role="presentation" className="slds-lookup__item--label slds-text-body--small">Recent Items</h3>
-              </li>
-              <li role="presentation">
-                <span className="slds-lookup__item-action slds-media" id="option-01" role="option">
-                  <SvgIcon className="slds-icon slds-icon-standard-opportunity slds-icon--small slds-media__figure" sprite="standard" symbol="opportunity" />
-                  <span className="slds-media__body">
-                    <span className="slds-lookup__result-text">Salesforce - 1,000 Licenses</span>
-                    <span className="slds-lookup__result-meta slds-text-body--small">Opportunity &bull; Prospecting</span>
-                  </span>
-                </span>
-              </li>
-              <li role="presentation">
-                <span className="slds-lookup__item-action slds-media" id="option-02" role="option">
-                  <SvgIcon className="slds-icon slds-icon-standard-contact slds-icon--small slds-media__figure" sprite="standard" symbol="contact" />
-                  <span className="slds-media__body">
-                    <span className="slds-lookup__result-text">Art Vandelay</span>
-                    <span className="slds-lookup__result-meta slds-text-body--small">Contact &bull; avandelay@vandelay.com</span>
-                  </span>
-                </span>
-              </li>
-              <li role="presentation">
-                <span className="slds-lookup__item-action slds-media" id="option-03" role="option">
-                  <SvgIcon className="slds-icon slds-icon-standard-account slds-icon--small slds-media__figure" sprite="standard" symbol="account" />
-                  <span className="slds-media__body">
-                    <span className="slds-lookup__result-text">Vandelary Industries</span>
-                    <span className="slds-lookup__result-meta slds-text-body--small">Account &bull; San Francisco</span>
-                  </span>
-                </span>
-              </li>
-              <li role="presentation">
-                <span className="slds-lookup__item-action slds-media" id="option-04" role="option">
-                  <SvgIcon className="slds-icon slds-icon-custom-8 slds-icon--small slds-media__figure" sprite="custom" symbol="custom8" />
-                  <span className="slds-media__body">
-                    <span className="slds-lookup__result-text">Salesforce UK 2016 Events</span>
-                    <span className="slds-lookup__result-meta slds-text-body--small">General Ledger &bull; $20,000</span>
-                  </span>
-                </span>
-              </li>
-              <li role="presentation">
-                <span className="slds-lookup__item-action slds-media" id="option-05" role="option">
-                  <SvgIcon className="slds-icon slds-icon-standard-lead slds-icon--small slds-media__figure" sprite="standard" symbol="lead" />
-                  <span className="slds-media__body">
-                    <span className="slds-lookup__result-text">H.E. Pennypacker</span>
-                    <span className="slds-lookup__result-meta slds-text-body--small">Lead &bull; Nursing</span>
-                  </span>
-                </span>
-              </li>
-            </ul>
-          </div>
-        </GlobalHeader>
+        <GlobalHeader expanded />
       </div>,
     script: `
       document.getElementById('global-search-01').focus()
@@ -103,81 +37,7 @@ export let states = [
     label: 'Typeahead',
     element:
       <div className="demo-only" style={{ height: '340px' }}>
-      <GlobalHeader autocomplete>
-        <div className="slds-form-element__control slds-input-has-icon slds-input-has-icon--left-right">
-          <SvgIcon className="slds-input__icon slds-input__icon--left" sprite="utility" symbol="search" />
-          <input
-            id="global-search-01"
-            className="slds-input slds-lookup__search-input"
-            type="search"
-            placeholder="Search Salesforce"
-            aria-owns="global-search-list-01"
-            role="combobox"
-            aria-activedescendant="option-00"
-            aria-expanded="true"
-            aria-autocomplete="list"
-            defaultValue="ibm" />
-          <button className="slds-input__icon slds-input__icon--right slds-button slds-button--icon">
-            <SvgIcon className="slds-button__icon" sprite="utility" symbol="clear" />
-            <span className="slds-assistive-text">Clear the current search term</span>
-          </button>
-        </div>
-        <div className="slds-lookup__menu" role="listbox" id="global-search-list-01">
-          <ul className="slds-lookup__list" role="presentation">
-            <li role="presentation">
-              <span className="slds-lookup__item-action slds-lookup__item-action--label slds-text-body--small slds-has-focus" id="option-00" role="option">
-                <SvgIcon className="slds-icon slds-icon--x-small slds-icon-text-default" sprite="utility" symbol="search" />
-                <span className="slds-truncate">"ibm" in Salesforce</span>
-              </span>
-            </li>
-            <li role="presentation">
-              <span className="slds-lookup__item-action slds-media" id="option-01" role="option">
-                <SvgIcon className="slds-icon slds-icon-standard-opportunity slds-icon--small slds-media__figure" sprite="standard" symbol="opportunity" />
-                <span className="slds-media__body">
-                  <span className="slds-lookup__result-text"><mark>IBM</mark> - 1yr/100k</span>
-                  <span className="slds-lookup__result-meta slds-text-body--small">Opportunity &bull; Proposal/Quote</span>
-                </span>
-              </span>
-            </li>
-            <li role="presentation">
-              <span className="slds-lookup__item-action slds-media" id="option-02" role="option">
-                <SvgIcon className="slds-icon slds-icon-standard-account slds-icon--small slds-media__figure" sprite="standard" symbol="account" />
-                <span className="slds-media__body">
-                  <span className="slds-lookup__result-text"><mark>IBM</mark></span>
-                  <span className="slds-lookup__result-meta slds-text-body--small">Account  &bull; Menlo Park</span>
-                </span>
-              </span>
-            </li>
-            <li role="presentation">
-              <span className="slds-lookup__item-action slds-media" id="option-03" role="option">
-                <SvgIcon className="slds-icon slds-icon-standard-account slds-icon--small slds-media__figure" sprite="standard" symbol="account" />
-                <span className="slds-media__body">
-                  <span className="slds-lookup__result-text"><mark>IBM</mark> Watson</span>
-                  <span className="slds-lookup__result-meta slds-text-body--small">Account  &bull; Menlo Park</span>
-                </span>
-              </span>
-            </li>
-            <li role="presentation">
-              <span className="slds-lookup__item-action slds-media" id="option-04" role="option">
-                <SvgIcon className="slds-icon slds-icon-standard-opportunity slds-icon--small slds-media__figure" sprite="standard" symbol="opportunity" />
-                <span className="slds-media__body">
-                  <span className="slds-lookup__result-text">200 Service Licenses - <mark>IBM</mark></span>
-                  <span className="slds-lookup__result-meta slds-text-body--small">Opportunity  &bull; Close-Won</span>
-                </span>
-              </span>
-            </li>
-            <li role="presentation">
-              <span className="slds-lookup__item-action slds-media" id="option-05" role="option">
-                <SvgIcon className="slds-icon slds-icon-standard-contact slds-icon--small slds-media__figure" sprite="standard" symbol="contact" />
-                <span className="slds-media__body">
-                  <span className="slds-lookup__result-text">Art Vandelay (<mark>IBM</mark>)</span>
-                  <span className="slds-lookup__result-meta slds-text-body--small">User &bull; Latex Salesman</span>
-                </span>
-              </span>
-            </li>
-          </ul>
-        </div>
-      </GlobalHeader>
+      <GlobalHeader expanded searchingFor="ibm" />
       </div>,
     script: `
       document.getElementById('global-search-01').focus()

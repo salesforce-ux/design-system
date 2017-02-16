ad330c00d185d2048a5564a0646f09eef8f56ba3
Kaelig <kaelig@deloumeau.fr>
2017-02-16 15:49:56 -0800

    Add license headers to root files

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
+// Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
+
 // Karma configuration
 // Generated on Tue Mar 24 2015 16:05:39 GMT-0400 (EDT)
 

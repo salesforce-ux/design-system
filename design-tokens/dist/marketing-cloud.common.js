65e25f8c98d098b294023e31a849d763eb12d704
Heroku <slds-ops@salesforce.com>
2017-02-21 21:13:07 +0000

    "Initial commit"

---
+++ design-tokens/dist/marketing-cloud.common.js
@@ -0,0 +1,52 @@
+module.exports = {
+  // Use for large headings only.
+  fontWeightLight: "300",
+  // Most all body copy.
+  fontWeightRegular: "400",
+  // Used sparingly for emphasized text within regular body copy.
+  fontWeightBold: "700",
+  fontSizeXSmall: "0.625rem",
+  fontSizeSmall: "0.875rem",
+  fontSizeMedium: "1rem",
+  fontSizeMediumA: "1.125rem",
+  fontSizeLarge: "1.25rem",
+  fontSizeXLarge: "1.5rem",
+  fontSizeXLargeA: "1.57rem",
+  fontSizeXxLarge: "2rem",
+  // Extra extra small text.
+  fontSizeTextXxSmall: ".625rem",
+  // Extra small body text.
+  fontSizeTextXSmall: ".75rem",
+  // Small body text.
+  fontSizeTextSmall: ".8125rem",
+  // Medium body text.
+  fontSizeTextMedium: "1rem",
+  // Large body text.
+  fontSizeTextLarge: "1.125rem",
+  // Extra large body text.
+  fontSizeTextXLarge: "1.25rem",
+  // Extra Extra small headings.
+  fontSizeHeadingXxSmall: ".625rem",
+  // Extra small headings.
+  fontSizeHeadingXSmall: ".75rem",
+  // Small headings.
+  fontSizeHeadingSmall: ".875rem",
+  // Medium headings.
+  fontSizeHeadingMedium: "1.125rem",
+  // Large headings.
+  fontSizeHeadingLarge: "1.5rem",
+  // Extra large headings.
+  fontSizeHeadingXLarge: "2rem",
+  // Unitless line-heights for reusability
+  lineHeightHeading: "1.25",
+  // Unitless line-heights for reusability
+  lineHeightText: "1.375",
+  // Remove extra leading. Unitless line-heights for reusability
+  lineHeightReset: "1",
+  // Line heights for tabs
+  lineHeightTab: "2.5rem",
+  // Deprecated: use FONT_FAMILY_TEXT instead for regular body text and FONT_FAMILY_HEADING for headings.
+  fontFamily: "'Salesforce Sans', Arial, sans-serif",
+  fontFamilyText: "'Salesforce Sans', Arial, sans-serif",
+  fontFamilyHeading: "'Salesforce Sans', Arial, sans-serif"
+};

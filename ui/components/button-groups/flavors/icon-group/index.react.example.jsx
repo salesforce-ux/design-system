169cd08e1c6d076abf0c33409c403fb100e5d99b
Brandon Ferrua <bferrua@gmail.com>
2017-03-02 14:43:31 -0800

    Chore: Add aria-pressed to toggle buttons on button icon group (#2119)

--- ui/components/button-groups/flavors/icon-group/index.react.example.jsx
+++ ui/components/button-groups/flavors/icon-group/index.react.example.jsx
@@ -6,24 +6,6 @@ import { ButtonGroup } from 'ui/components/button-groups/flavors/base/index.reac
 import { ButtonIcon } from 'ui/components/button-icons/flavors/base/index.react.example';
 
 ///////////////////////////////////////////
-// State Constructor(s)
-///////////////////////////////////////////
-
-let Default = props =>
-  <ButtonGroup>
-    <ButtonIcon className="slds-button--icon-border" symbol="chart" assistiveText="Charts" title="Charts" />
-    <ButtonIcon className="slds-button--icon-border" symbol="filterList" assistiveText="Filter List" title="Filter List" />
-    <ButtonIcon className="slds-button--icon-more" hasDropdown assitiveText="More Actions" title="More Actions" />
-  </ButtonGroup>;
-
-let Selected = props =>
-  <ButtonGroup>
-    <ButtonIcon className="slds-button--icon-border slds-is-selected" symbol="chart" assistiveText="Charts" title="Charts" />
-    <ButtonIcon className="slds-button--icon-border" symbol="filterList" assistiveText="Filter List" title="Filter List" />
-    <ButtonIcon className="slds-button--icon-more" hasDropdown assitiveText="More Actions" title="More Actions" />
-  </ButtonGroup>;
-
-///////////////////////////////////////////
 // Export
 ///////////////////////////////////////////
 
@@ -31,11 +13,55 @@ export let states = [
   {
     id: 'button-group-icon',
     label: 'Default',
-    element: <Default />
+    element:
+      <ButtonGroup>
+        <ButtonIcon
+          className="slds-button--icon-border"
+          symbol="chart"
+          assistiveText="Charts"
+          title="Charts"
+          aria-pressed="false"
+        />
+        <ButtonIcon
+          className="slds-button--icon-border"
+          symbol="filterList"
+          assistiveText="Filter List"
+          title="Filter List"
+          aria-pressed="false"
+        />
+        <ButtonIcon
+          className="slds-button--icon-more"
+          hasDropdown={true}
+          assitiveText="More Actions"
+          title="More Actions"
+        />
+      </ButtonGroup>
   },
   {
     id: 'button-group-icon-selected',
     label: 'Selected',
-    element: <Selected />
+    element:
+      <ButtonGroup>
+        <ButtonIcon
+          className="slds-button--icon-border slds-is-selected"
+          symbol="chart"
+          assistiveText="Charts"
+          title="Charts"
+          aria-pressed="true"
+        />
+        <ButtonIcon
+          className="slds-button--icon-border"
+          symbol="filterList"
+          assistiveText="Filter List"
+          title="Filter List"
+          aria-pressed="false"
+        />
+        <ButtonIcon
+          className="slds-button--icon-more"
+          hasDropdown={true}
+          assitiveText="More Actions"
+          title="More Actions"
+        />
+      </ButtonGroup>
   }
 ];

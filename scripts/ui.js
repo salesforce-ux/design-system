e71f51246523bc4daac4be92690a67b3386ccff5
Brian Lonsdorf <brian@looprecur.com>
2017-03-10 11:11:53 -0800

    update dist
    all should be ready to build the new site
    
    update headers

---
+++ scripts/ui.js
@@ -0,0 +1,46 @@
+const createParser = require('@salesforce-ux/design-system-parser');
+const Task = require('data.task');
+const {List, Map} = require('immutable');
+const {getMarkup, getComments} = require('./markup-style');
+require('./helpers/setup');
+
+const createUI = parser =>
+  Map({components: parser.component,  utilities: parser.utility})
+  .map((fn, group) =>
+    parser[group]()
+    .reduce((ac, c) =>
+      ac.set(c.get('id'), fn(c.get('id')).get()), Map()));
+
+const requireMarkup = name => x =>
+  x.set('markup', getMarkup(name, x.get('id')).getOrElse(List()));
+
+const requireIfVariant = name => x =>
+  x.getIn(['annotations', 'variant'])
+  ? requireMarkup(name)(x)
+  : x;
+
+const mapTree = (x, f) =>
+  f(x).set('restrictees', x.get('restrictees', []).map(r => mapTree(r, f)));
+
+const reduceTree = (f, empty, x) =>
+  f(x.get('restrictees', [])
+  .reduce((ac, y) =>
+    reduceTree(f, ac, y), empty), x);
+
+const foldMap = (f, empty, x) =>
+  reduceTree((acc, y) => acc.concat(f(y)), empty, x);
+
+const addMarkup = (group, f) => ui =>
+  ui
+  .update(group, cs =>
+    cs.map((value, name) =>
+      mapTree(value, f(name))));
+
+const ui = () =>
+  getComments()
+  .map(createParser)
+  .map(createUI)
+  .map(addMarkup('components', requireIfVariant))
+  .map(addMarkup('utilities', requireMarkup));
+
+module.exports = {ui, mapTree, foldMap};

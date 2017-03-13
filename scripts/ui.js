14b8913d336eb819dd58538822d1c64c48a3af16
Brian Lonsdorf <brian@looprecur.com>
2017-03-13 09:21:13 -0700

    add license header

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
+// Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
+
 const createParser = require('@salesforce-ux/design-system-parser');
 const Task = require('data.task');
 const {List, Map} = require('immutable');

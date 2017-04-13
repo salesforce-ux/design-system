0880fc7ac2f0b4d4ae93cda959e5a26d7d5ec9fe
Brandon Ferrua <bferrua@gmail.com>
2017-04-13 13:19:14 -0700

    chore(modal): update examples (#2342)

--- ui/components/modals/base/example.jsx
+++ ui/components/modals/base/example.jsx
@@ -54,25 +54,6 @@ export let ModalFooter = props =>
 // State Constructor(s)
 /// ///////////////////////////////////////////
 
-let Default = props =>
-  <Demo style={{height: '640px'}}>
-    <Modal aria-labelledby="modal-heading-01">
-      <ModalHeader>
-        <h2 id="modal-heading-01" className="slds-text-heading--medium">Modal Header</h2>
-      </ModalHeader>
-      <ModalContent className="slds-p-around--medium">
-        <p>Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt aute id consequat veniam incididunt duis in sint irure nisi. Mollit officia cillum Lorem ullamco minim nostrud elit officia tempor esse quis. Cillum sunt ad dolore
-          quis aute consequat ipsum magna exercitation reprehenderit magna. Tempor cupidatat consequat elit dolor adipisicing.</p>
-        <p>Dolor eiusmod sunt ex incididunt cillum quis nostrud velit duis sit officia. Lorem aliqua enim laboris do dolor eiusmod officia. Mollit incididunt nisi consectetur esse laborum eiusmod pariatur proident. Eiusmod et adipisicing culpa deserunt
-          nostrud ad veniam nulla aute est. Labore esse esse cupidatat amet velit id elit consequat minim ullamco mollit enim excepteur ea.</p>
-      </ModalContent>
-      <ModalFooter>
-        <button className="slds-button slds-button--neutral">Cancel</button>
-        <button className="slds-button slds-button--brand">Save</button>
-      </ModalFooter>
-    </Modal>
-  </Demo>;
-
 let Taglines = props =>
   <Demo style={{height: '640px'}}>
     <Modal aria-labelledby="modal-heading-01">
@@ -191,26 +172,11 @@ export default (
   </Demo>
 );
 
-export let states = [
-  {
-    id: 'default',
-    label: 'Default',
-    element: <Default />
-  },
+export let examples = [
   {
     id: 'taglines',
     label: 'Taglines',
     element: <Taglines />
-  },
-  {
-    id: 'large',
-    label: 'Large',
-    element: <Large />
-  },
-  {
-    id: 'directional',
-    label: 'Directional',
-    element: <Directional />
   },
   {
     id: 'headless',

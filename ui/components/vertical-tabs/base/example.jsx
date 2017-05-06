7550f01d2e41f1cabff0b3cf4722dc87cddb4fec
Jade Pennig <jpennig@salesforce.com>
2017-05-05 19:57:33 -0700

    First pass on base variant for the new Vertical Tabs component.

---
+++ ui/components/vertical-tabs/base/example.jsx
@@ -0,0 +1,116 @@
+// ui/components/vertical-tab/base/example.jsx
+
+// Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
+// Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
+
+import React from 'react';
+import classNames from 'classnames';
+
+class VerticalTabs extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      currentTabIndex: 0
+    }
+
+    this.changeTab = this.changeTab.bind(this);
+  }
+
+  changeTab(event) {
+    // todo...
+  }
+
+  render() {
+    const { currentTabIndex } = this.state;
+
+    return (
+      <div className='slds-vertical-tabs'>
+        <ul className='slds-vertical-tabs__nav' role='tablist'>
+          {this.props.tabs.map((tab, index) => {
+            const isActive = index === currentTabIndex ? 'slds-is-active' : '';
+            const tabIndex = index === currentTabIndex ? 0 : -1;
+            const isAriaSelected = index === currentTabIndex ? 'true' : 'false';
+            const ariaControlId = `slds-vertical-tabs-${index}`;
+            const tabNavId = `slds-vertical-tabs-${index}__nav`;
+
+            return (
+              <li 
+                className={classNames('slds-vertical-tabs__nav-item', isActive)} 
+                title={tab.label} 
+                role='presentation' 
+                key={tabNavId} 
+              >
+                <a 
+                  className='slds-vertical-tabs__link' 
+                  href='javascript:void(0)'
+                  role='tab' 
+                  tabIndex={tabIndex} 
+                  aria-selected={isAriaSelected}
+                  aria-controls={ariaControlId}
+                  id={tabNavId} 
+                  onClick={this.handleTabClick}
+                >
+                  {tab.label}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+
+        {this.props.tabs.map((tab, index) => {
+          const tabContentId = `slds-vertical-tabs-${index}`;
+          const showHideClass = index === currentTabIndex ? 'slds-show' : 'slds-hide';
+          const ariaLabelledBy = `slds-vertical-tabs-${index}__nav`;
+
+          return (
+            <div 
+              className={classNames('slds-vertical-tabs__content', showHideClass)}
+              id={tabContentId}
+              role='tabpanel'
+              aria-labelledby={ariaLabelledBy}
+              key={tabContentId}
+            >
+              {this.props.tabs[currentTabIndex].content}
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+}
+
+let exampleTabs = [{
+  label: 'Tab 1',
+  content: (
+    <div className='slds-text-longform'>
+      <p>Content for Tab 1</p>
+      <p>Lorem ipsum dolor...</p>
+      <p>Lorem ipsum dolor...</p>
+    </div>
+  )
+},{
+  label: 'Tab 2',
+  content: (
+    <div className='slds-text-longform'>
+      <p>Content for Tab 2</p>
+    </div>
+  )
+},{
+  label: 'Tab 3',
+  content: (
+    <div className='slds-text-longform'>
+      <p>Content for Tab 3</p>
+    </div>
+  )
+},];
+
+export default (
+  <VerticalTabs tabs={exampleTabs} />
+);
+
+// export const examples = [{
+//   id: 'Living Example',
+//   label: 'Living Example',
+//   element: <VerticalTabs tabs={exampleTabs} />
+// }];

78df4633ee4825f1ce42e178441ff7669c970384
Stephanie Rewis <srewis@salesforce.com>
2017-05-12 12:44:02 -0700

    Fix(Notifications): Updated a11y to match popovers, changed name

--- ui/components/notifications/base/example.jsx
+++ ui/components/notifications/base/example.jsx
@@ -2,6 +2,7 @@
 // Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
 
 import React from 'react';
+import _ from 'lodash';
 import { ButtonIcon } from '../../button-icons/base/example';
 import { StandardIcon } from '../../icons/standard/example';
 import SvgIcon from '../../../shared/svg-icon';
@@ -11,25 +12,34 @@ import classNames from 'classnames';
 // State Constructor(s)
 /// ///////////////////////////////////////////
 
-let Reminder = props => {
-  const {
-    className,
-    children,
-    ...rest
-  } = props;
+let Notification = props => {
+  const headingUniqueId = _.uniqueId('dialog-heading-id-');
+  const bodyUniqueId = _.uniqueId('dialog-body-id-');
 
   return (
-    <div className="slds-reminder_container">
-      <a className="slds-reminder slds-media" role="dialog">
-        { children }
-      </a>
-      <ButtonIcon
-        className="slds-button--icon-container slds-reminder__close"
-        symbol="close"
-        assistiveText="Close"
-        title="Close"
+    <section
+      className="slds-notification"
+      role="dialog"
+      aria-labelledby={!props.header && props.headerTitle ? headingUniqueId : props.headingId}
+      aria-label={!props.header && !props.headerTitle ? props.title : null}
+      aria-describedby={bodyUniqueId}>
+      <div className="slds-notification__body" id={bodyUniqueId}>
+        <a className="slds-notification__target slds-media" href="javascript:void(0);">
+          { props.children }
+        </a>
+        <ButtonIcon
+          className="slds-button--icon-container slds-notification__close"
+          symbol="close"
+          assistiveText="Close"
+          title="Close"
         />
-    </div>
+      </div>
+        { props.footer
+        ? <footer className={classNames('slds-notification__footer', props.footerClassName)}>
+          { props.footer }
+        </footer>
+      : null }
+    </section>
   );
 };
 
@@ -43,7 +53,7 @@ export const Context = props =>
   </div>;
 
 export default (
-  <Reminder>
+  <Notification>
     <StandardIcon
       containerClassName="slds-media__figure"
       className="slds-icon--small"
@@ -54,15 +64,15 @@ export default (
       <h2 className="slds-text-heading--small slds-m-bottom--xx-small">Tesla - Renewal meeting</h2>
       <p>Event at 11:00am on Jan 8</p>
     </div>
-  </Reminder>
+  </Notification>
 );
 
 export let examples = [
   {
-    id: 'task-reminder',
-    label: 'Task Reminder',
+    id: 'task-notification',
+    label: 'Task Notification',
     element:
-      <Reminder>
+      <Notification>
         <StandardIcon
           containerClassName="slds-media__figure"
           className="slds-icon--small"
@@ -73,6 +83,6 @@ export let examples = [
           <h2 className="slds-text-heading--small slds-m-bottom--xx-small">Call Two: Jane Johnson</h2>
           <p>Task due on Jan 8</p>
         </div>
-      </Reminder>
+      </Notification>
   }
 ];

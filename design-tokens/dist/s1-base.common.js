a3a156dc25f7e396a41f42461ba348a2f9424530
Heroku <slds-ops@salesforce.com>
2017-05-12 16:59:44 +0000

    "Initial commit"

---
+++ design-tokens/dist/s1-base.common.js
@@ -0,0 +1,34 @@
+module.exports = {
+  widthStageLeft: "88%",
+  // Action footer menu. Phone 44px; Tablet 50px
+  heightFooter: "3.75rem",
+  // Brand header. Phone 44px; Tablet 50px
+  heightHeader: "2.75rem",
+  // Text input height
+  heightInput: "1.875rem",
+  // Space above overflow menu before scrolling.
+  heightOverflowStart: "144px",
+  // Maximum height for photos and such in cards.
+  maxHeightCardMedia: "144px",
+  // Maximum height for brand logos in header.
+  maxHeightHeaderBrand: "32px",
+  // Maximum width for action icons in action bar when there are only 2 items with labels.
+  maxWidthActionBarWithLabels: "320px",
+  // Maximum width for action overflow menu so that it's not full-screen for tablet.
+  maxWidthActionOverflowMenu: "512px",
+  // Maximum width for brand logos in header.
+  maxWidthHeaderBrand: "156px",
+  // Use squareIconLargeBoundary
+  squareRecordAnchorPic: "3rem",
+  // Tap target Size
+  squareTappable: "2.75rem",
+  // 2 Column Layout - Master
+  flexMasterPanel: "7",
+  // Tooltip square size
+  squareTooltip: "1.25rem",
+  // Tooltip nubbin square size
+  squareTooltipNubbin: "1rem",
+  // 2 Column Layout - Detail
+  flexDetailPanel: "3",
+  durationSlide: "0.25s"
+};

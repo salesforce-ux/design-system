873b9bf63495a85dab332d342d1ff4084c52f6ca
Jade Pennig <jpennig@salesforce.com>
2017-06-02 20:53:48 -0500

    Add svg icon support. Implement known states with existing iconography.

--- ui/components/progress-ring/base/example.jsx
+++ ui/components/progress-ring/base/example.jsx
@@ -3,6 +3,7 @@
 
 import React from 'react';
 import ProgressRing from '../';
+import SvgIcon from '../../../shared/svg-icon';
 
 export default (<ProgressRing percent={88} />);
 
@@ -10,16 +11,40 @@ export const states = [
   {
     id: 'progress-ring-warning',
     label: 'Warning State',
-    element: <ProgressRing percent={42} />
+    element: (
+      <ProgressRing percent={22}>
+        <SvgIcon
+          className="slds-icon slds-icon_xx-small"
+          sprite="utility"
+          symbol="warning"
+        />
+      </ProgressRing>
+    )
   },
   {
     id: 'progress-ring-expired',
     label: 'Expired State',
-    element: <ProgressRing percent={0} />
+    element: (
+      <ProgressRing percent={0}>
+        <SvgIcon
+          className="slds-icon slds-icon_xx-small"
+          sprite="utility"
+          symbol="error"
+        />
+      </ProgressRing>
+    )
   },
   {
     id: 'progress-ring-complete',
     label: 'Complete State',
-    element: <ProgressRing percent={100} />
+    element: (
+      <ProgressRing percent={100}>
+        <SvgIcon
+          className="slds-icon slds-icon_xx-small"
+          sprite="utility"
+          symbol="check"
+        />
+      </ProgressRing>
+    )
   }
-]
+];

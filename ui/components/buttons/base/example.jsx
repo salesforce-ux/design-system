28b6de3ded84d418ff2b3995b1dd75506664dc5b
Brian Lonsdorf <brian@looprecur.com>
2017-06-26 15:55:21 -0700

    button examples

--- ui/components/buttons/base/example.jsx
+++ ui/components/buttons/base/example.jsx
@@ -20,6 +20,32 @@ export default (
 
 export let states = [
   {
+    id: 'neutral',
+    label: 'Neutral',
+    element: <Button className="slds-button_neutral">Button</Button>
+  },
+  {
+    id: 'brand',
+    label: 'Brand',
+    element: <Button className="slds-button_brand">Button</Button>
+  },
+  {
+    id: 'destructive',
+    label: 'Desctructive',
+    element: <Button className="slds-button_destructive">Button</Button>
+  },
+  {
+    id: 'inverse',
+    label: 'Inverse',
+    element: <Button className="slds-button_inverse">Button</Button>,
+    Context: <div style={{padding: '0.5rem', background: '#16325c'}} />
+  },
+  {
+    id: 'success',
+    label: 'Success',
+    element: <Button className="slds-button_success">Button</Button>
+  },
+  {
     id: 'disabled',
     label: 'Disabled',
     element: <Button disabled>Button</Button>

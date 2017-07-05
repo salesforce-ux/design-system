ff27ec0bc239ad5946505d0bddf40f2dbe9fbadd
Matthew Craig <matthew.craig@ncino.com>
2017-07-05 16:35:02 -0400

    PP-1659 removed un-needed file

--- build_artifact.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-RELEASE_VERSION=$(cat release.txt)
-release-helper -d create_release -o loanlifecycle -r design-system -n  --body "This is an automated release of v$RELEASE_VERSION"  -t $RELEASE_VERSION -T $GITHUB_PERSONAL_TOKEN
-

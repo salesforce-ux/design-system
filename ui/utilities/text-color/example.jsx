3342309202dfd220a6ba205499dea92736a71cc5
Jade Pennig <jpennig@salesforce.com>
2017-07-13 16:44:52 -0500

    feature(color-utils): add color palette tokens and add text color utility classes

---
+++ ui/utilities/text-color/example.jsx
@@ -0,0 +1,108 @@
+// Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
+// Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
+
+import React from 'react';
+
+export let examples = [
+  {
+    id: 'color-1',
+    label: 'Text Color 1',
+    element: (
+      <div className="demo-only" style={{ background: '#333230', padding: '10px' }}>
+        <p className="slds-text-color_gray-1">This is text color gray 1</p>
+      </div>
+    )
+  },
+  {
+    id: 'color-2',
+    label: 'Text Color 2',
+    element: (
+      <div className="demo-only" style={{ background: '#333230', padding: '10px' }}>
+        <p className="slds-text-color_gray-2">This is text color gray 2</p>
+      </div>
+    )
+  },
+  {
+    id: 'color-3',
+    label: 'Text Color 3',
+    element: (
+      <div className="demo-only" style={{ background: '#333230', padding: '10px' }}>
+        <p className="slds-text-color_gray-3">This is text color gray 3</p>
+      </div>
+    )
+  },
+  {
+    id: 'color-4',
+    label: 'Text Color 4',
+    element: (
+      <div className="demo-only" style={{ background: '#333230', padding: '10px' }}>
+        <p className="slds-text-color_gray-4">This is text color gray 4</p>
+      </div>
+    )
+  },
+  {
+    id: 'color-5',
+    label: 'Text Color 5',
+    element: (
+      <div className="demo-only" style={{ background: '#333230', padding: '10px' }}>
+        <p className="slds-text-color_gray-5">This is text color gray 5</p>
+      </div>
+    )
+  },
+  {
+    id: 'color-6',
+    label: 'Text Color 6',
+    element: (
+      <div className="demo-only" style={{ background: '#333230', padding: '10px' }}>
+        <p className="slds-text-color_gray-6">This is text color gray 6</p>
+      </div>
+    )
+  },
+  {
+    id: 'color-7',
+    label: 'Text Color 7',
+    element: (
+      <div className="demo-only" style={{ background: '#333230', padding: '10px' }}>
+        <p className="slds-text-color_gray-7">This is text color gray 7</p>
+      </div>
+    )
+  },
+  {
+    id: 'color-8',
+    label: 'Text Color 8',
+    element: (
+      <p className="slds-text-color_gray-8">This is text color gray 8</p>
+    )
+  },
+  {
+    id: 'color-9',
+    label: 'Text Color 9',
+    element: (
+      <p className="slds-text-color_gray-9">This is text color gray 9</p>
+    )
+  },
+  {
+    id: 'color-10',
+    label: 'Text Color 10',
+    element: (
+      <p className="slds-text-color_gray-10">This is text color gray 10</p>
+    )
+  },
+  {
+    id: 'color-11',
+    label: 'Text Color 11',
+    element: (
+      <p className="slds-text-color_gray-11">This is text color gray 11</p>
+    )
+  },
+  {
+    id: 'color-12',
+    label: 'Text Color 12',
+    element: (
+      <p className="slds-text-color_gray-12">This is text color gray 12</p>
+    )
+  },
+];
+
+
+

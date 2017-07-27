c3cfcf8e2a55b836676e40bdf6bb2b6cd985d416
Jade Pennig <jpennig@salesforce.com>
2017-07-27 14:37:35 -0700

    feat(color-utils): add svg fill classes, final ui appearance for util docs

--- ui/utilities/color/example.jsx
+++ ui/utilities/color/example.jsx
@@ -2,30 +2,9 @@
 // Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
 
 import React from 'react';
+import SvgIcon from '../../shared/svg-icon';
 
-const paletteValues = [1,2,3,4,5,6,7,8,9,10,11,12];
-const textGroups = [
-  {
-    title: 'Medium dark or darker background',
-    background: 10,
-    colors: [1,2,3,4]
-  },
-  {
-    title: 'Dark background only',
-    background: 12,
-    colors: [5,6,7,8]
-  },
-  {
-    title: 'Light background only',
-    background: 0,
-    colors: [9,10]
-  },
-  {
-    title: 'Medium-Light or lighter background',
-    background: 6,
-    colors: [11,12]
-  }
-];
+const paletteKeys = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 export let examples = [
   {
@@ -35,12 +14,12 @@ export let examples = [
       <div>
         <h2 className="slds-text-heading_large slds-m-bottom_medium">Background Colors</h2>
         <div className="slds-m-bottom_large">
-          {paletteValues.map((value, index) => {
-            const textClass = value > 8 ? 'slds-color__text_gray-1' : null
+          {paletteKeys.map((key, index) => {
+            const textClass = key > 8 ? 'slds-color__text_gray-1' : null
 
             return (
-              <div key={index} className={`slds-color__background_gray-${value} slds-p-around_medium`}>
-                <p className={textClass}>.slds-color__background_gray-{value}</p>
+              <div key={index} className={`slds-color__background_gray-${key} slds-p-around_medium`}>
+                <p className={textClass}>.slds-color__background_gray-{key}</p>
               </div>
             )
           })}
@@ -48,36 +27,44 @@ export let examples = [
 
         <h2 className="slds-text-heading_large slds-m-bottom_medium">Text Colors</h2>
         <div className="slds-m-bottom_large">
-          {textGroups.map((group, groupIndex) => (
-            <div key={groupIndex} className="demo-only-colors slds-m-bottom_medium">
-              <h3 className="slds-text-heading_medium slds-m-bottom_small">
-                {group.title}
-              </h3>
-              
-              <div className={`slds-color__background_gray-${group.background}`}>
-                {group.colors.map((color, colorIndex) => (
-                  <div key={colorIndex} className={`slds-color__text_gray-${color} slds-p-around_medium`}>
-                    .slds-color__text_gray-{color}
-                  </div>
-                ))}
-              </div>
+          {paletteKeys.map((key, index) => (
+            <div className="slds-grid slds-grid_vertical-align-center">
+              <div className={`slds-color__background_gray-${key}`} style={{
+                height: '3rem',
+                width: '3rem',
+                display: 'inline-block'
+              }} />
+              <p className="slds-p-left_small">.slds-color__text_gray-{key}</p>
             </div>
           ))}
         </div>
 
         <h2 className="slds-text-heading_large slds-m-bottom_medium">Border Colors</h2>
         <div className="slds-m-bottom_large">
-          {paletteValues.map((value, index) => (
+          {paletteKeys.map((key, index) => (
             <div
               key={index}
-              className={`slds-color__border_gray-${value} slds-p-around_medium slds-m-bottom_small`}
+              className={`slds-color__border_gray-${key} slds-p-around_medium slds-m-bottom_small`}
               style={{'border-width': '1px', 'border-style': 'solid'}}
             >
-              .slds-color__border_gray-{value}
+              .slds-color__border_gray-{key}
             </div>
           ))}
         </div>
 
+        <h2 className="slds-text-heading_large slds-m-bottom_medium">SVG Fill Colors</h2>
+        <div className="slds-m-bottom_large">
+          {paletteKeys.map((key, index) => (
+            <div className="slds-p-around_small">
+              <SvgIcon
+                className={`slds-color__fill_gray-${key} slds-icon_small`}
+                sprite="action"
+                symbol="check"
+              />
+              <span className="slds-p-left_small">.slds-color__fill_gray-{key}</span>
+            </div>
+          ))}
+        </div>
       </div>
     )
   }

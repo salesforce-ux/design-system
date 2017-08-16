817f6f24dcebb17d9450f7a4f0a5f43b4bd2be84
Brian Lonsdorf <brian@looprecur.com>
2017-08-16 16:15:02 -0700

    initial

---
+++ design-tokens/dist/force-font-commons.common.js
@@ -0,0 +1,12 @@
+module.exports = {
+  salesforceSansRegularWoff2: "/_slds/fonts/v2.3.0/SalesforceSans-Regular.woff2",
+  salesforceSansRegularWoff: "/_slds/fonts/v2.3.0/SalesforceSans-Regular.woff",
+  salesforceSansLightWoff2: "/_slds/fonts/v2.3.0/SalesforceSans-Light.woff2",
+  salesforceSansLightWoff: "/_slds/fonts/v2.3.0/SalesforceSans-Light.woff",
+  salesforceSansBoldWoff2: "/_slds/fonts/v2.3.0/SalesforceSans-Bold.woff2",
+  salesforceSansBoldWoff: "/_slds/fonts/v2.3.0/SalesforceSans-Bold.woff",
+  salesforceSansBoldItalicWoff2: "/_slds/fonts/v2.3.0/SalesforceSans-BoldItalic.woff2",
+  salesforceSansBoldItalicWoff: "/_slds/fonts/v2.3.0/SalesforceSans-BoldItalic.woff",
+  salesforceSansItalicWoff2: "/_slds/fonts/v2.3.0/SalesforceSans-Italic.woff2",
+  salesforceSansItalicWoff: "/_slds/fonts/v2.3.0/SalesforceSans-Italic.woff"
+};

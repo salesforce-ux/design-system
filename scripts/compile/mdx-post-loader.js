a735a4be31865aba44d05a62d7ce99795aad13f7
Brian Lonsdorf <brian@looprecur.com>
2017-08-21 15:24:37 -0700

    feature(doc-blocks): add doc blocks [Finished: #147154327]

---
+++ scripts/compile/mdx-post-loader.js
@@ -0,0 +1,7 @@
+module.exports = code => {
+  return `
+    import Doc from '../../../shared/components/Doc'
+    ${code.replace(/export default function/, 'export const Content = function')}
+    export default props => <Doc><Content {...props} /></Doc>
+  `;
+};

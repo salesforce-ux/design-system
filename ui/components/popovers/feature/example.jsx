08b2aaca3bd5f81dfc6b6cbcfa5cf2e138f0a881
Ed Ngai <edngai3@gmail.com>
2017-10-03 12:49:40 -0700

    fix(popovers): update background and styling of walkthrough popovers (#2792)
    * fix(popovers): update background and styling of walkthrough popovers
    
    * fix(popovers): add new nubbin colors
    
    * fix(popovers): add feature variant, tests, and tokens
    
    * chore(popovers): delete snapshots
    
    * chore(popovers): add tests and specify port
    
    * chore(popovers): extend Footer component in walkthrough
    
    * chore(popovers): skip tests until port issue is resolved
    
    * chore(popovers): remove title and add back tests
    
    * chore(popovers): update snapshots
    
    * chore(popovers): skip tests until hyphens issue is resolved

---
+++ ui/components/popovers/feature/example.jsx
@@ -0,0 +1,183 @@
+// Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
+// Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
+
+import React from 'react';
+import SvgIcon from '../../../shared/svg-icon';
+import { ButtonIcon } from '../../button-icons/base/example';
+import { Popover } from '../base/example';
+import { Footer } from '../walkthrough/example';
+import classNames from 'classnames';
+
+const headingUniqueId = 'dialog-heading-id-01';
+
+/// ///////////////////////////////////////////
+// Export
+/// ///////////////////////////////////////////
+
+export default (
+  <Popover
+    className="slds-popover_walkthrough slds-popover_feature slds-nubbin_left"
+    headingId={headingUniqueId}
+    closeButton
+    inverse
+  >
+    <div className="slds-media">
+      <div className="slds-media__body">
+        <h2 id={headingUniqueId} className="slds-text-heading_small">
+          Shelly, it seems you frequent this record. Try favoriting it for easy
+          access.
+        </h2>
+      </div>
+    </div>
+  </Popover>
+);
+
+export let examples = [
+  {
+    id: 'icon-text',
+    label: 'With icon and text',
+    element: (
+      <Popover
+        className="slds-popover_walkthrough slds-popover_feature slds-nubbin_left"
+        headingId={headingUniqueId}
+        closeButton
+        inverse
+      >
+        <div className="slds-media">
+          <div className="slds-media__figure">
+            <span
+              className="slds-icon_container"
+              title="description of icon when needed"
+            >
+              <SvgIcon
+                className="slds-icon slds-icon_small slds-icon-text-default"
+                sprite="utility"
+                symbol="favorite"
+              />
+              <span className="slds-assistive-text">Description of icon</span>
+            </span>
+          </div>
+          <div className="slds-media__body">
+            <h2 id={headingUniqueId} className="slds-text-heading_small">
+              Shelly, it seems you frequent this record. Try favoriting it for
+              easy access.
+            </h2>
+          </div>
+        </div>
+      </Popover>
+    )
+  },
+  {
+    id: 'icon-header-text-link',
+    label: 'With icon, header, text, and link',
+    element: (
+      <Popover
+        className="slds-popover_walkthrough slds-popover_feature slds-nubbin_left"
+        headingId={headingUniqueId}
+        closeButton
+        inverse
+      >
+        <div className="slds-media">
+          <div className="slds-media__figure">
+            <span
+              className="slds-icon_container"
+              title="description of icon when needed"
+            >
+              <SvgIcon
+                className="slds-icon slds-icon_small slds-icon-text-default"
+                sprite="utility"
+                symbol="smiley_and_people"
+              />
+              <span className="slds-assistive-text">Description of icon</span>
+            </span>
+          </div>
+          <div className="slds-media__body">
+            <h2 id={headingUniqueId} className="slds-text-heading_small">
+              Title
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.{' '}
+              <a>Learn more</a>
+            </p>
+          </div>
+        </div>
+      </Popover>
+    )
+  },
+  {
+    id: 'icon-header-text',
+    label: 'With icon, header, and text',
+    element: (
+      <Popover
+        className="slds-popover_walkthrough slds-popover_feature slds-nubbin_left"
+        headingId={headingUniqueId}
+        closeButton
+        inverse
+      >
+        <div className="slds-media">
+          <div className="slds-media__figure">
+            <span
+              className="slds-icon_container"
+              title="description of icon when needed"
+            >
+              <SvgIcon
+                className="slds-icon slds-icon_small slds-icon-text-default"
+                sprite="utility"
+                symbol="description"
+              />
+              <span className="slds-assistive-text">Description of icon</span>
+            </span>
+          </div>
+          <div className="slds-media__body">
+            <h2 id={headingUniqueId} className="slds-text-heading_small">
+              Title
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+      </Popover>
+    )
+  },
+  {
+    id: 'icon-header-text-footer',
+    label: 'With icon, header, text, and footer',
+    element: (
+      <Popover
+        className="slds-popover_walkthrough slds-popover_feature slds-nubbin_left"
+        headingId={headingUniqueId}
+        footer={<Footer learnMoreButton />}
+        closeButton
+        inverse
+      >
+        <div className="slds-media">
+          <div className="slds-media__figure">
+            <span
+              className="slds-icon_container"
+              title="description of icon when needed"
+            >
+              <SvgIcon
+                className="slds-icon slds-icon_small slds-icon-text-default"
+                sprite="utility"
+                symbol="favorite"
+              />
+              <span className="slds-assistive-text">Description of icon</span>
+            </span>
+          </div>
+          <div className="slds-media__body">
+            <h2 id={headingUniqueId} className="slds-text-heading_small">
+              Title
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+      </Popover>
+    )
+  }
+];

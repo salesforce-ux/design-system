10cf83457e5a284bc954343e907de752103a180a
Ayesha Mazumdar <a.mazumdar@salesforce.com>
2017-10-20 16:10:12 -0700

    feat(activity-timeline): Added new version of activity timeline that expands and collapses items (#2820)
    * feat(activity-timeline): Added support for icons in title
    
    * feat(activity-timeline): Added capability for footer buttons
    
    * feat(activity-timeline): Updated markup to support narrow width
    
    * feat(activity-timeline): Added docs and Accessibility implementation notes

---
+++ ui/components/activity-timeline/__tests__/index.spec.jsx
@@ -0,0 +1,327 @@
+// Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
+// Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
+/* eslint-env jest */
+
+import React from 'react';
+import createHelpers from '../../../../jest.setup';
+import { render, mount } from 'enzyme';
+import { UtilityIcon } from '../../icons/base/example';
+import {
+  TimelineIcon,
+  TimelineActions,
+  TimelineMediaFigure,
+  TimelineMediaBody,
+  TimelineError,
+  TimelineTrigger,
+  TimelineSubtext,
+  TimelineItem,
+  DescriptionCard
+} from '../base/example';
+import {
+  ActivityTimelineItemCall,
+  ActivityTimelineItemTask,
+  ActivityTimelineItemEmail,
+  ActivityTimelineItemEvent,
+  BaseExample as StaticExample
+} from '../deprecated/example';
+
+const { matchesMarkupAndStyle } = createHelpers(__dirname);
+
+const emailIcons = [
+  <UtilityIcon
+    key="groups"
+    className="slds-icon_xx-small slds-icon-text-default slds-m-left_x-small"
+    symbol="groups"
+    title="Group email"
+    assistiveText="Group email"
+  />,
+  <UtilityIcon
+    key="attach"
+    className="slds-icon_xx-small slds-icon-text-default slds-m-left_x-small"
+    symbol="attach"
+    title="Has attachments"
+    assistiveText="Has attachments"
+  />
+];
+
+describe('Activity Timeline Expandable', () => {
+  xit('renders a hover state', () => {
+    const wrapper = mount(
+      <TimelineItem
+        id="call-item"
+        type="log_a_call"
+        title="Mobile conversation on Monday"
+        date="10:00am | 3/23/17"
+        subtext={{
+          you: 'You',
+          action: 'logged a call with',
+          to: 'Adam Chan'
+        }}
+      />
+    );
+    return matchesMarkupAndStyle(
+      wrapper
+        .find('.slds-timeline__trigger')
+        .at(0)
+        .simulate('mouseEnter')
+        .getNode()
+    );
+  });
+
+  it('renders a timeline icon', () =>
+    matchesMarkupAndStyle(<TimelineIcon symbol="email" />));
+
+  it('renders a collapsed timeline media figure', () =>
+    matchesMarkupAndStyle(
+      <TimelineMediaFigure id="event-figure" type="event" isExpanded="false" />
+    ));
+
+  it('renders an expanded timeline media figure', () =>
+    matchesMarkupAndStyle(
+      <TimelineMediaFigure id="event-figure" type="event" isExpanded="true" />
+    ));
+
+  it('renders a timeline media body', () =>
+    matchesMarkupAndStyle(
+      <TimelineMediaBody>
+        <TimelineTrigger type="email" title="EBC Follow Up Details" />
+        <TimelineSubtext
+          subtext={{
+            you: 'You',
+            action: 'created a task with',
+            to: 'Charlie Gomez'
+          }}
+        />
+      </TimelineMediaBody>
+    ));
+
+  it('renders a timeline item trigger with no icons', () =>
+    matchesMarkupAndStyle(
+      <TimelineTrigger type="email" title="EBC Follow Up Details" />
+    ));
+
+  it('renders a timeline item trigger with icons', () =>
+    matchesMarkupAndStyle(
+      <TimelineTrigger
+        type="email"
+        title="EBC Follow Up Details"
+        iconSet={emailIcons}
+      />
+    ));
+
+  it('renders a timeline item trigger with a task checkbox', () =>
+    matchesMarkupAndStyle(
+      <TimelineTrigger type="task" title="Send EBC Follow Up Details" />
+    ));
+
+  it('renders simple timeline subtext', () =>
+    matchesMarkupAndStyle(
+      <TimelineSubtext
+        subtext={{
+          you: 'You',
+          action: 'created a task with',
+          to: 'Charlie Gomez'
+        }}
+      />
+    ));
+
+  it('renders timeline subtext with extra text at the end', () =>
+    matchesMarkupAndStyle(
+      <TimelineSubtext
+        subtext={{
+          you: 'You',
+          action: 'created a task with',
+          to: 'Charlie Gomez',
+          extra: 'and 4 others'
+        }}
+      />
+    ));
+
+  it('renders a timeline item error', () =>
+    matchesMarkupAndStyle(<TimelineError />));
+
+  it('renders timeline actions', () =>
+    matchesMarkupAndStyle(
+      <TimelineActions
+        date="9:00am | 2/24/2017"
+        title="More Options for Call, Mobile conversation"
+        assistiveText="More Options for Call, Mobile conversation"
+      />
+    ));
+
+  it('renders a timeline item', () =>
+    matchesMarkupAndStyle(
+      <TimelineItem
+        id="call-item"
+        type="log_a_call"
+        title="Mobile conversation on Monday"
+        date="10:00am | 3/23/17"
+        subtext={{
+          you: 'You',
+          action: 'logged a call with',
+          to: 'Adam Chan'
+        }}
+        fields={[
+          { label: 'Name', content: 'Adam Chan' },
+          {
+            label: 'Related To',
+            content: 'Tesla Cloudhub + Anypoint Connectors'
+          }
+        ]}
+        bodyLabel="Description"
+      >
+        Adam seemed interested in closing this deal quickly! Let’s move.
+      </TimelineItem>
+    ));
+
+  it('renders a task timeline item', () =>
+    matchesMarkupAndStyle(
+      <TimelineItem
+        id="call-item"
+        type="task"
+        title="Mobile conversation on Monday"
+        date="10:00am | 3/23/17"
+        subtext={{
+          you: 'You',
+          action: 'created a task with',
+          to: 'Adam Chan'
+        }}
+        fields={[
+          { label: 'Name', content: 'Adam Chan' },
+          {
+            label: 'Related To',
+            content: 'Tesla Cloudhub + Anypoint Connectors'
+          }
+        ]}
+        bodyLabel="Description"
+      >
+        Adam seemed interested in closing this deal quickly! Let’s move.
+      </TimelineItem>
+    ));
+
+  it('renders a timeline item with icons', () =>
+    matchesMarkupAndStyle(
+      <TimelineItem
+        id="call-item"
+        type="task"
+        title="Mobile conversation on Monday"
+        date="10:00am | 3/23/17"
+        subtext={{
+          you: 'You',
+          action: 'created a task with',
+          to: 'Adam Chan'
+        }}
+        iconSet={emailIcons}
+        fields={[
+          { label: 'Name', content: 'Adam Chan' },
+          {
+            label: 'Related To',
+            content: 'Tesla Cloudhub + Anypoint Connectors'
+          }
+        ]}
+        bodyLabel="Description"
+      >
+        Adam seemed interested in closing this deal quickly! Let’s move.
+      </TimelineItem>
+    ));
+
+  it('renders a narrow timeline item', () =>
+    matchesMarkupAndStyle(
+      <TimelineItem
+        id="call-item"
+        type="log_a_call"
+        title="Mobile conversation on Monday"
+        date="10:00am | 3/23/17"
+        subtext={{
+          you: 'You',
+          action: 'logged a call with',
+          to: 'Adam Chan'
+        }}
+        fields={[
+          { label: 'Name', content: 'Adam Chan' },
+          {
+            label: 'Related To',
+            content: 'Tesla Cloudhub + Anypoint Connectors'
+          }
+        ]}
+        bodyLabel="Description"
+        isNarrow
+      >
+        Adam seemed interested in closing this deal quickly! Let’s move.
+      </TimelineItem>
+    ));
+
+  it('renders an expanded timeline item', () =>
+    matchesMarkupAndStyle(
+      <TimelineItem
+        id="call-item"
+        type="log_a_call"
+        title="Mobile conversation on Monday"
+        date="10:00am | 3/23/17"
+        subtext={{
+          you: 'You',
+          action: 'logged a call with',
+          to: 'Adam Chan'
+        }}
+        fields={[
+          { label: 'Name', content: 'Adam Chan' },
+          {
+            label: 'Related To',
+            content: 'Tesla Cloudhub + Anypoint Connectors'
+          }
+        ]}
+        bodyLabel="Description"
+        isExpanded="true"
+      >
+        Adam seemed interested in closing this deal quickly! Let’s move.
+      </TimelineItem>
+    ));
+
+  it('renders an empty details section', () =>
+    matchesMarkupAndStyle(<DescriptionCard id="test1" />));
+
+  it('renders an expanded empty details section', () =>
+    matchesMarkupAndStyle(<DescriptionCard id="test1" isExpanded="true" />));
+
+  it('renders a full expanded details section without footer buttons', () =>
+    matchesMarkupAndStyle(
+      <DescriptionCard
+        id="test-id"
+        isExpanded="true"
+        fields={[
+          { label: 'Name', content: 'Lei Chan' },
+          {
+            label: 'Related To',
+            content: 'Tesla Cloudhub + Anypoint Connectors'
+          }
+        ]}
+        bodyLabel="Description"
+      >
+        Hi folks, Thanks for meeting with the team today and going through the
+        proposals we saw. This goes on until it is truncated.
+      </DescriptionCard>
+    ));
+
+  it('renders a full expanded details section with footer buttons', () =>
+    matchesMarkupAndStyle(
+      <DescriptionCard
+        id="test-id"
+        isExpanded="true"
+        fields={[
+          { label: 'Name', content: 'Lei Chan' },
+          {
+            label: 'Related To',
+            content: 'Tesla Cloudhub + Anypoint Connectors'
+          }
+        ]}
+        bodyLabel="Description"
+        footerButtons
+      >
+        Hi folks, Thanks for meeting with the team today and going through the
+        proposals we saw. This goes on until it is truncated.
+      </DescriptionCard>
+    ));
+
+  it('renders the deprecated combined timeline', () =>
+    matchesMarkupAndStyle(<StaticExample />));
+});
--- ui/components/activity-timeline/base/example.jsx
+++ ui/components/activity-timeline/base/example.jsx
@@ -3,20 +3,65 @@
 
 import React from 'react';
 import { ButtonIcon } from '../../button-icons/base/example';
-import { ActivityTimelineItem as Event } from '../event/example';
-import { ActivityTimelineItem as Call } from '../call/example';
-import { ActivityTimelineItem as Email } from '../email/example';
-import { ActivityTimelineItem as Task } from '../task/example';
+import { Checkbox } from '../../checkbox/base/example';
 import SvgIcon from '../../../shared/svg-icon';
+import { UtilityIcon } from '../../icons/base/example';
+import { Button } from '../../buttons/base/example';
 import classNames from 'classnames';
 
-export let TimelineIcon = props => (
+const emailItemId = 'email-item';
+const callItemId = 'call-item';
+const eventItemId = 'event-item';
+const taskItemId = 'task-item';
+
+const emailIcons = [
+  <UtilityIcon
+    key="groups"
+    className="slds-icon_xx-small slds-icon-text-default slds-m-left_x-small"
+    symbol="groups"
+    title="Group email"
+    assistiveText="Group email"
+  />,
+  <UtilityIcon
+    key="attach"
+    className="slds-icon_xx-small slds-icon-text-default slds-m-left_x-small"
+    symbol="attach"
+    title="Has attachments"
+    assistiveText="Has attachments"
+  />
+];
+const taskIcons = [
+  <UtilityIcon
+    key="rotate"
+    className="slds-icon_xx-small slds-icon-text-default slds-m-left_x-small"
+    symbol="rotate"
+    title="Recurring Task"
+    assistiveText="Recurring Task"
+  />
+];
+const eventIcons = [
+  <UtilityIcon
+    key="world"
+    className="slds-icon_xx-small slds-icon-text-default slds-m-left_x-small"
+    symbol="world"
+    title="Public sharing"
+    assistiveText="Public sharing"
+  />
+];
+
+/// ///////////////////////////////////////////
+// Partial(s)
+/// ///////////////////////////////////////////
+
+/* Timeline Components */
+export const TimelineIcon = props => (
   <div
     className={classNames(
       'slds-icon_container',
       props.symbol == 'log_a_call'
         ? 'slds-icon-standard-log-a-call'
-        : 'slds-icon-standard-' + props.symbol
+        : 'slds-icon-standard-' + props.symbol,
+      props.className
     )}
     title={props.symbol == 'log_a_call' ? 'call' : props.symbol}
   >
@@ -28,8 +73,12 @@ export let TimelineIcon = props => (
   </div>
 );
 
-export let TimelineActions = props => (
-  <div className="slds-timeline__actions">
+export const TimelineActions = props => (
+  <div
+    className={classNames('slds-timeline__actions', {
+      'slds-timeline__actions_inline': props.inline
+    })}
+  >
     <p className="slds-timeline__date">{props.date || 'Feb 24'}</p>
     <ButtonIcon
       className="slds-button_icon-border-filled slds-button_icon-x-small"
@@ -41,19 +90,306 @@ export let TimelineActions = props => (
   </div>
 );
 
-export default (
+export const TimelineMediaFigure = props => (
+  <div className="slds-media__figure">
+    <ButtonIcon
+      iconClassName="slds-timeline__details-action-icon"
+      symbol="switch"
+      title={`Toggle details for ${props.title}`}
+      assistiveText={`Toggle details for ${props.title}`}
+      aria-controls={props.id}
+      aria-expanded={props.isExpanded}
+    />
+    <TimelineIcon symbol={props.type} className="slds-timeline__icon" />
+  </div>
+);
+
+export const TimelineMediaBody = props => (
+  <div className="slds-media__body">{props.children}</div>
+);
+
+export const TimelineTrigger = props => (
+  <div className="slds-grid slds-grid_align-spread slds-timeline__trigger">
+    <div className="slds-grid slds-grid_vertical-align-center slds-truncate_container_75 slds-no-space">
+      {props.type === 'task' && (
+        <Checkbox hideLabel label={`Mark ${props.title} complete`} />
+      )}
+      <h3 className="slds-truncate" title={props.title}>
+        <a href="javascript:void(0);">
+          <strong>{props.title}</strong>
+        </a>
+      </h3>
+      {props.iconSet && <div className="slds-no-flex">{props.iconSet}</div>}
+    </div>
+    <TimelineActions
+      inline
+      date={props.date}
+      title="More Options for this item"
+      assistiveText="More Options for this item"
+    />
+  </div>
+);
+
+export const TimelineSubtext = props => (
+  <p className="slds-m-horizontal_xx-small">
+    <a href="javascript:void(0);">{props.subtext.you}</a> {props.subtext.action}{' '}
+    <a href="javascript:void(0);">{props.subtext.to}</a> {props.subtext.extra}
+  </p>
+);
+
+export const TimelineError = props => (
+  <div
+    className="slds-grid slds-text-color_error slds-m-top_x-small slds-m-horizontal_xx-small"
+    id={props.id}
+  >
+    <UtilityIcon
+      key="attach"
+      className="slds-icon-text-error slds-icon_x-small slds-m-right_xx-small"
+      symbol="error"
+      title="Error Occurred"
+      assistiveText="Error Occurred"
+    />
+    <p>There was an error loading the details</p>
+  </div>
+);
+
+export const TimelineItem = props => (
+  <div
+    className={classNames(
+      'slds-timeline__item_expandable',
+      `${props.type === 'log_a_call'
+        ? 'slds-timeline__item_call'
+        : 'slds-timeline__item_' + props.type}`,
+      { 'slds-is-open': props.isExpanded === 'true' },
+      props.className
+    )}
+  >
+    <span className="slds-assistive-text">{props.type}</span>
+    <div className={classNames('slds-media')}>
+      <TimelineMediaFigure {...props} />
+      <TimelineMediaBody>
+        <TimelineTrigger {...props} />
+        <TimelineSubtext {...props} />
+        {props.hasError ? (
+          <TimelineError {...props} />
+        ) : (
+          <DescriptionCard
+            id={props.id}
+            isExpanded={props.isExpanded}
+            fields={props.fields}
+            bodyLabel={props.bodyLabel}
+            footerButtons={props.footerButtons}
+          >
+            {props.children}
+          </DescriptionCard>
+        )}
+      </TimelineMediaBody>
+    </div>
+  </div>
+);
+
+export let FieldInfoListItem = props => (
+  <li className="slds-grid slds-grid--vertical slds-size_1-of-2 slds-truncate slds-p-bottom_small">
+    <span className="slds-text-title slds-p-bottom_x-small">{props.label}</span>
+    <span className="slds-text-body_medium slds-truncate" title={props.content}>
+      <a href="javascript:void(0);">{props.content}</a>
+    </span>
+  </li>
+);
+
+/* Description Card Component */
+export let FieldInfoList = props => (
+  <ul className="slds-list_horizontal slds-wrap">
+    {props.fields.map((field, i) => (
+      <FieldInfoListItem key={i} label={field.label} content={field.content} />
+    ))}
+  </ul>
+);
+
+export const DescriptionCard = props => (
+  <article
+    className={classNames(
+      'slds-box',
+      'slds-timeline__item_details',
+      'slds-theme_shade',
+      'slds-m-top_x-small',
+      'slds-m-horizontal_xx-small',
+      { 'slds-p-around_medium': props.isExpanded === 'true' },
+      props.className
+    )}
+    id={props.id}
+    aria-hidden={props.isExpanded === 'false'}
+  >
+    {props.fields && <FieldInfoList {...props} />}
+    {props.children && (
+      <div>
+        <span className="slds-text-title">{props.bodyLabel}</span>
+        <p className="slds-p-top_x-small">{props.children}</p>
+      </div>
+    )}
+    {props.footerButtons && (
+      <Button className="slds-button_neutral slds-m-top_small">
+        <SvgIcon
+          className="slds-button__icon slds-button__icon_left"
+          sprite="utility"
+          symbol="world"
+        />
+        Public Sharing
+      </Button>
+    )}
+  </article>
+);
+
+export const NarrowContext = props => (
+  <div className="demo-only" style={{ width: '400px' }}>
+    {props.children}
+  </div>
+);
+
+export let BaseExample = props => (
   <ul className="slds-timeline">
     <li>
-      <Task />
+      <TimelineItem
+        id={`${taskItemId + '-' + props.id}`}
+        type="task"
+        title="Review proposals for EBC deck with larger team and have marketing review this"
+        date="9:00am | 3/20/17"
+        subtext={{
+          you: 'You',
+          action: 'created a task with',
+          to: 'Charlie Gomez'
+        }}
+        iconSet={taskIcons}
+        fields={[
+          { label: 'Name', content: 'Charlie Gomez' },
+          {
+            label: 'Related To',
+            content: 'Tesla Cloudhub + Anypoint Connectors'
+          }
+        ]}
+        bodyLabel="Description"
+        isExpanded={props.isExpanded}
+        isNarrow={props.isNarrow}
+        hasError={props.hasError}
+      >
+        Need to finalize proposals and brand details before the meeting
+      </TimelineItem>
     </li>
     <li>
-      <Event />
+      <TimelineItem
+        id={`${callItemId + '-' + props.id}`}
+        type="log_a_call"
+        title="Mobile conversation on Monday"
+        date="10:00am | 3/23/17"
+        subtext={{
+          you: 'You',
+          action: 'logged a call with',
+          to: 'Adam Chan'
+        }}
+        fields={[
+          { label: 'Name', content: 'Adam Chan' },
+          {
+            label: 'Related To',
+            content: 'Tesla Cloudhub + Anypoint Connectors'
+          }
+        ]}
+        bodyLabel="Description"
+        isExpanded={props.isExpanded}
+        isNarrow={props.isNarrow}
+      >
+        Adam seemed interested in closing this deal quickly! Let’s move.
+      </TimelineItem>
     </li>
     <li>
-      <Call />
+      <TimelineItem
+        id={`${emailItemId + '-' + props.id}`}
+        type="email"
+        title="Re: Mobile conversation on Monday with the new global team"
+        date="10:00am | 3/24/17"
+        subtext={{
+          you: 'You',
+          action: 'emailed',
+          to: 'Lea Chan'
+        }}
+        iconSet={emailIcons}
+        fields={[
+          { label: 'From Address', content: 'Jackie Dewar' },
+          {
+            label: 'To Address',
+            content: 'Lea Chan'
+          }
+        ]}
+        bodyLabel="Text Body"
+        isExpanded={props.isExpanded}
+        isNarrow={props.isNarrow}
+        footerButtons={props.footerButtons}
+      >
+        Hi everyone, Thanks for meeting with the team today and going through
+        the proposals we saw. This goes on and wraps if needed.
+      </TimelineItem>
     </li>
     <li>
-      <Email />
+      <TimelineItem
+        id={`${eventItemId + '-' + props.id}`}
+        type="event"
+        title="EBC Follow up call"
+        date="10:30am | 3/24/17"
+        subtext={{
+          you: 'You',
+          action: 'created an event with',
+          to: 'Aida Lee',
+          extra: 'and 5 others'
+        }}
+        iconSet={eventIcons}
+        fields={[
+          {
+            label: 'Location',
+            content: 'Westen St. Francis, San Francisco, CA, 94622'
+          },
+          {
+            label: 'Attendees',
+            content: 'Jason Dewar (Organizer) + 5 others'
+          },
+          {
+            label: 'When',
+            content: 'March 26, 10:00 AM - 11:00 AM'
+          }
+        ]}
+        bodyLabel="Description"
+        isExpanded={props.isExpanded}
+        isNarrow={props.isNarrow}
+        footerButtons={props.footerButtons}
+      >
+        Let's discuss the 2017 product roadmap and address any questions
+      </TimelineItem>
     </li>
   </ul>
 );
+
+export default <BaseExample id="base" isExpanded="false" />;
+
+export let examples = [
+  {
+    id: 'narrow',
+    label: 'Narrow',
+    element: (
+      <NarrowContext>
+        <BaseExample id="narrow" isExpanded="true" isNarrow footerButtons />
+      </NarrowContext>
+    )
+  }
+];
+
+export let states = [
+  {
+    id: 'expanded',
+    label: 'Expanded',
+    element: <BaseExample id="expanded" isExpanded="true" footerButtons />
+  },
+
+  {
+    id: 'error',
+    label: 'Error',
+    element: <BaseExample id="error" isExpanded="true" hasError footerButtons />
+  }
+];

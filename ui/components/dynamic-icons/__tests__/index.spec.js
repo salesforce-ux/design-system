79980e47465d3d67d66d3e51275f5b09640ce07e
Si Taggart <me@simontaggart.com>
2017-10-23 13:25:46 -0700

    feat(dynamic icon) Create typing dynamic icon variant to indicate a user is typing

---
+++ ui/components/dynamic-icons/__tests__/index.spec.js
@@ -0,0 +1,30 @@
+/* eslint-env jest */
+import React from 'react';
+
+import { TypingIcon } from '../typing/example';
+
+import createHelpers from '../../../../jest.setup';
+
+const { matchesMarkupAndStyle } = createHelpers(__dirname);
+
+describe('Typing icon', () => {
+  it('renders a typing icon', () =>
+    matchesMarkupAndStyle(
+      <TypingIcon title="User typing" assistiveText="User typing" />
+    ));
+
+  xit('renders an animated typing icon', () =>
+    matchesMarkupAndStyle(
+      <TypingIcon isAnimated title="User typing" assistiveText="User typing" />
+    ));
+
+  it('renders a paused animated typing icon', () =>
+    matchesMarkupAndStyle(
+      <TypingIcon
+        isAnimated
+        isPaused
+        title="User typing"
+        assistiveText="User typing"
+      />
+    ));
+});

f0483d29d930973153845d451b7af6e9e8e378b2
Brian Lonsdorf <brian@trnsfr.com>
2017-11-01 13:15:26 -0700

    feature(badges): new documentation (#2755)
    * feature(badges): new documentation
    
    * fix(icon badges): update icon
    
    * fix(badges): Cleaned up docblocks, add additional tests for icons

--- ui/components/badges/__tests__/index.spec.js
+++ ui/components/badges/__tests__/index.spec.js
@@ -1,10 +1,8 @@
 /* eslint-env jest */
 import React from 'react';
-
 import { LightBadge, InverseBadge, Badge } from '../';
-
+import { UtilityIcon } from '../../icons/base/example';
 import createHelpers from '../../../../jest.setup';
-
 const { matchesMarkupAndStyle } = createHelpers(__dirname);
 
 it('renders a default badge', () =>
@@ -13,5 +11,44 @@ it('renders a default badge', () =>
 it('renders an inverse badge', () =>
   matchesMarkupAndStyle(<InverseBadge>Lovely badge</InverseBadge>));
 
-it('renders an a light badge', () =>
-  matchesMarkupAndStyle(<LightBadge>Light</LightBadge>));
+describe('renders a light badge', () => {
+  it('renders badge', () =>
+    matchesMarkupAndStyle(<LightBadge>Light</LightBadge>));
+
+  it('renders badge with left icon', () =>
+    matchesMarkupAndStyle(
+      <LightBadge>
+        <UtilityIcon
+          containerClassName="slds-m-right_xx-small"
+          className="slds-icon_xx-small slds-icon-text-default"
+          assistiveText={false}
+          symbol="world"
+        />
+        Icon on the left
+      </LightBadge>
+    ));
+
+  it('renders badge with right icon', () =>
+    matchesMarkupAndStyle(
+      <LightBadge>
+        <UtilityIcon
+          containerClassName="slds-m-right_xx-small"
+          className="slds-icon_xx-small slds-icon-text-default"
+          assistiveText={false}
+          symbol="world"
+        />
+        Icon on the right
+      </LightBadge>
+    ));
+
+  it('renders badge with icon only', () =>
+    matchesMarkupAndStyle(
+      <LightBadge>
+        <UtilityIcon
+          className="slds-icon_xx-small slds-icon-text-default"
+          assistiveText="assistive text goes here, do it!"
+          symbol="world"
+        />
+      </LightBadge>
+    ));
+});

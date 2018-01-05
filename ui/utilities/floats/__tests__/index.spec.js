e7e76b00ebd61d339734ea51bd4024240a3efd33
Brandon Ferrua <bferrua@gmail.com>
2018-01-05 14:39:33 -0800

    chore(floats): Add test (#2985)

---
+++ ui/utilities/floats/__tests__/index.spec.js
@@ -0,0 +1,43 @@
+/* eslint-env jest */
+import React from 'react';
+import createHelpers from '../../../../jest.setup';
+const { matchesMarkupAndStyle } = createHelpers(__dirname);
+
+describe('renders float utilities', () => {
+  it('renders float left', () =>
+    matchesMarkupAndStyle(
+      <div className="slds-clearfix">
+        <div className="slds-float_left">
+          <p>I’m floooaaaating</p>
+        </div>
+      </div>
+    ));
+
+  it('renders float right', () =>
+    matchesMarkupAndStyle(
+      <div className="slds-clearfix">
+        <div className="slds-float_right">
+          <p>I’m floooaaaating</p>
+        </div>
+      </div>
+    ));
+
+  it('renders float none', () =>
+    matchesMarkupAndStyle(
+      <div className="slds-float_none">
+        <p>I’m not floooaaaating</p>
+      </div>
+    ));
+
+  it('renders floats with a clearfix', () =>
+    matchesMarkupAndStyle(
+      <div className="slds-clearfix">
+        <div className="slds-float_left">
+          <p>I’m floooaaaating</p>
+        </div>
+        <div className="slds-float_right">
+          <p>I’m floooaaaating</p>
+        </div>
+      </div>
+    ));
+});

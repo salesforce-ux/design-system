2dd99d2fb60d24ab53d563523567e76730110cbd
Brandon Ferrua <bferrua@gmail.com>
2018-01-05 10:56:57 -0800

    chore(borders): Add test (#2980)

---
+++ ui/utilities/borders/__tests__/index.spec.js
@@ -0,0 +1,28 @@
+/* eslint-env jest */
+import React from 'react';
+import createHelpers from '../../../../jest.setup';
+const { matchesMarkupAndStyle } = createHelpers(__dirname);
+
+describe('renders border utilities', () => {
+  it('renders top border', () =>
+    matchesMarkupAndStyle(
+      <div className="slds-border_top">This should have a top border.</div>
+    ));
+
+  it('renders right border', () =>
+    matchesMarkupAndStyle(
+      <div className="slds-border_right">This should have a right border.</div>
+    ));
+
+  it('renders bottom border', () =>
+    matchesMarkupAndStyle(
+      <div className="slds-border_bottom">
+        This should have a bottom border.
+      </div>
+    ));
+
+  it('renders left border', () =>
+    matchesMarkupAndStyle(
+      <div className="slds-border_left">This should have a left border.</div>
+    ));
+});

23f8684c1c65c857f81e34b73c5fd5c61efa732c
Ed Ngai <edmund.ngai@salesforce.com>
2018-01-16 16:19:26 -0800

    chore(checkbox-button): add test (#3022)

---
+++ ui/components/checkbox-button/__tests__/index.spec.js
@@ -0,0 +1,16 @@
+/* eslint-env jest */
+import React from 'react';
+import { CheckboxAddButton } from '../base/example';
+
+import createHelpers from '../../../../jest.setup';
+
+const { matchesMarkupAndStyle } = createHelpers(__dirname);
+
+it('renders a default checkbox button', () =>
+  matchesMarkupAndStyle(<CheckboxAddButton />));
+
+it('renders a checked checkbox button', () =>
+  matchesMarkupAndStyle(<CheckboxAddButton checked />));
+
+it('renders a disabled checkbox button', () =>
+  matchesMarkupAndStyle(<CheckboxAddButton disabled />));

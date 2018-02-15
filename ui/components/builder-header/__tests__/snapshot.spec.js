3b6f7419934cb14e3e238fa798afe8de943d5e04
Brandon Ferrua <bferrua@gmail.com>
2018-02-15 10:26:21 -0800

    feat(builder header): Add component (#3079)
    * feat(builder-header): Add base structure
    
    * feat(builder-header): Add test and restriction rules
    
    * Lint
    
    * Lint
    
    * feat(builder-header): Add accessibility reqs for toolbar
    
    * feat(builder-header): Remove AT and title from button icons that already have text
    
    * feat(builder-header): Update snapshots
    
    * feat(builder-header): Update tabindex on button groups

---
+++ ui/components/builder-header/__tests__/snapshot.spec.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
+// Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
+
+/* eslint-env jest */
+
+import React from 'react';
+import createHelpers from '../../../../jest.setup';
+import BuilderHeader from '../';
+const { matchesMarkupAndStyle } = createHelpers(__dirname);
+
+describe('render builder header', () => {
+  it('render base builder header', () =>
+    matchesMarkupAndStyle(<BuilderHeader />));
+  it('render base builder header with toolbar', () =>
+    matchesMarkupAndStyle(<BuilderHeader showToolbar />));
+});

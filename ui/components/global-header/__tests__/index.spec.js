f5ae215750fb24088bd310ae0a33eacccc1599ba
Si Taggart <me@simontaggart.com>
2018-04-06 17:24:42 -0700

    chore(global header): Fix the global header tests

--- ui/components/global-header/__tests__/index.spec.js
+++ ui/components/global-header/__tests__/index.spec.js
@@ -1,19 +1,40 @@
 // Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
 // Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
 /* eslint-env jest */
-
 import React from 'react';
-import createHelpers from '../../../../jest.helpers';
+
 import { GlobalHeaderDeprecated } from '../base/example';
 import GlobalHeader from '../';
 
+import createHelpers from '../../../../jest.helpers';
+
 const { matchesMarkupAndStyle } = createHelpers(__dirname);
 
-describe('render global header', () => {
-  it('renders the deprecated global header', () => {
-    matchesMarkupAndStyle(<GlobalHeaderDeprecated />);
+describe('Global Header', () => {
+  it('renders a deprecated global header', () => {
+    return matchesMarkupAndStyle(<GlobalHeaderDeprecated />);
   });
-  it('render the global header', () => {
-    matchesMarkupAndStyle(<GlobalHeader />);
+  it('renders a deprecated global header with favorites selected', () => {
+    return matchesMarkupAndStyle(<GlobalHeaderDeprecated favoritesSelected />);
+  });
+  it('renders a deprecated global header with favorites disabled', () => {
+    return matchesMarkupAndStyle(<GlobalHeaderDeprecated favoritesDisabled />);
+  });
+  it('renders a deprecated global header with actions menu showing', () => {
+    return matchesMarkupAndStyle(<GlobalHeaderDeprecated actions />);
+  });
+  it('renders a deprecated global header with setup menu showing', () => {
+    return matchesMarkupAndStyle(<GlobalHeaderDeprecated setup />);
+  });
+  it('renders a deprecated global header with search expanded', () => {
+    return matchesMarkupAndStyle(<GlobalHeaderDeprecated expanded />);
+  });
+  it('renders a deprecated global header with search expanded and with search term', () => {
+    return matchesMarkupAndStyle(
+      <GlobalHeaderDeprecated expanded searchingFor="ibm" />
+    );
+  });
+  it('renders a global header', () => {
+    return matchesMarkupAndStyle(<GlobalHeader />);
   });
 });

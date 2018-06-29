3f64278b16b6f36c1458866f8671dbad19a5e501
Jade Pennig <zahnster@users.noreply.github.com>
2018-06-29 16:49:13 -0500

    chore(merge-summer-to-winter: merge branch 'summer-18' into winter-19 (#3367)

--- ui/utilities/margin/docs/Margin.jsx
+++ ui/utilities/margin/docs/Margin.jsx
@@ -2,42 +2,18 @@
 // Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
 
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom/server';
-import style from './Margin.scss';
+import './Margin.scss';
 import classNames from 'classnames';
 import CodeBlock from '../../../../shared/components/CodeBlock';
 
-const Redline = props => (
-  <span className="doc-redline" style={{ width: props.width }}>
-    <span />
-  </span>
-);
-
 class Margin extends Component {
   render() {
-    let margin;
     let { size, position } = this.props;
-    const sizes = {
-      'xxx-small': '0.125rem',
-      'xx-small': '0.25rem',
-      'x-small': '0.5rem',
-      small: '0.75rem',
-      medium: '1rem',
-      large: '1.5rem',
-      'x-large': '2rem',
-      'xx-large': '3rem'
-    };
-    let widths = JSON.stringify(sizes);
-    JSON.parse(widths, (key, value) => {
-      if (key === size) {
-        margin = value;
-      }
-    });
     let spacingClass = `slds-m-${position}_${size}`;
     return (
       <div className="docs-codeblock">
         <div className="docs-codeblock-example">
-          <div className={classNames('doc-spacing')}>
+          <div className="doc-spacing">
             <div
               className={classNames(
                 'doc-margin doc-margin_primary',

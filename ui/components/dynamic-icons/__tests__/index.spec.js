19d91ad7b901f57c3db72cb97f97f0142ef11e9e
Simon Taggart <me@simontaggart.com>
2018-09-27 14:09:08 -0700

    test(dynamic icons): unskip tests

--- ui/components/dynamic-icons/__tests__/index.spec.js
+++ ui/components/dynamic-icons/__tests__/index.spec.js
@@ -10,21 +10,21 @@ const { matchesMarkup } = createHelpers(__dirname);
 describe('Typing icon', () => {
   it('renders a typing icon', () =>
     matchesMarkup(
-      <TypingIcon title="User typing" assistiveText="User typing" />
+      <TypingIcon assistiveText="User typing" title="User typing" />
     ));
 
-  xit('renders an animated typing icon', () =>
+  it('renders an animated typing icon', () =>
     matchesMarkup(
-      <TypingIcon isAnimated title="User typing" assistiveText="User typing" />
+      <TypingIcon assistiveText="User typing" isAnimated title="User typing" />
     ));
 
   it('renders a paused animated typing icon', () =>
     matchesMarkup(
       <TypingIcon
+        assistiveText="User typing"
         isAnimated
         isPaused
         title="User typing"
-        assistiveText="User typing"
       />
     ));
 });

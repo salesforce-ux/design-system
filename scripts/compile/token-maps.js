71e664ade57c034eaf0dda7c9517859584bc51e3
John Earle <johnearle01@gmail.com>
2019-02-05 11:03:02 -0800

    feat(tokens): expose token scope to API (#3660)

--- scripts/compile/token-maps.js
+++ scripts/compile/token-maps.js
@@ -296,6 +296,7 @@ TokenMaps.removeDataInScss = scssComponentsMap =>
 TokenMaps.sanitizePath = path => path.replace(paths.root, '');
 
 TokenMaps.sanitizeRawToken = token => ({
+  scope: token.scope || 'global',
   category: token.category,
   comment: token.comment,
   type: token.type,

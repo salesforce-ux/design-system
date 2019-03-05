02e5cce33975f9a4ec5298ad3fc927406f7662af
Brandon Ferrua <bferrua@gmail.com>
2019-03-05 14:53:07 -0800

    Lint

--- ui/components/accordion/base/index.stories.js
+++ ui/components/accordion/base/index.stories.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { storiesOf } from '@storybook/react';
 import * as AccordionExamples from './example';
 import { getDisplayElementById } from '../../../shared/helpers';

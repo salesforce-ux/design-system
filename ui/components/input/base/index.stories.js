6065d1a347ed069136542462d6697d8f146beffa
Jesse Brack <jbrack@salesforce.com>
2019-05-24 14:43:05 -0400

    feat(input): increase tap target size (#3835)
    * feat(input): create mixin for components that rely on line-height for overall height
    Create standard token for touch height
    
    * feat(input): remove form-factor/small/sizing after height-input became deprecated
    add touch height to input
    
    * feat(input): fix how increment buttons are vertically centered
    New vertical-center mixin

--- ui/components/input/base/index.stories.js
+++ ui/components/input/base/index.stories.js
@@ -4,18 +4,28 @@ import { getDisplayElementById } from '../../../shared/helpers';
 
 import '../../../index.scss';
 
+storiesOf('Components/Input', module).add('Kitchen Sink', () => [
+  InputExamples.default,
+  InputExamples.examples.map(example =>
+    getDisplayElementById(InputExamples.examples, example.id)
+  ),
+  InputExamples.states.map(example =>
+    getDisplayElementById(InputExamples.states, example.id)
+  )
+]);
+
 const stories = storiesOf('Components/Input/Base', module).add(
   'Base',
   () => InputExamples.default
 );
 
-InputExamples.examples.forEach(example => {
+InputExamples.examples.map(example => {
   stories.add(example.label, () =>
     getDisplayElementById(InputExamples.examples, example.id)
   );
 });
 
-InputExamples.states.forEach(example => {
+InputExamples.states.map(example => {
   stories.add(example.label, () =>
     getDisplayElementById(InputExamples.states, example.id)
   );

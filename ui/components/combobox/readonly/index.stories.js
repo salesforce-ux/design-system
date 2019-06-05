62376a48c534f15b151dc8f04150e4612718ea01
Jesse Brack <jbrack@salesforce.com>
2019-06-04 20:02:03 -0400

    chore(combobox): add Storybook stories for combobox (#3860)

---
+++ ui/components/combobox/readonly/index.stories.js
@@ -0,0 +1,16 @@
+import { storiesOf } from '@storybook/react';
+import * as ReadonlyExamples from './example';
+import { getDisplayElementById } from '../../../shared/helpers';
+
+import '../../../index.scss';
+
+const stories = storiesOf('Components/Combobox/Readonly', module).add(
+  'Base',
+  () => ReadonlyExamples.default
+);
+
+ReadonlyExamples.states.map(example => {
+  stories.add(example.label, () =>
+    getDisplayElementById(ReadonlyExamples.states, example.id)
+  );
+});

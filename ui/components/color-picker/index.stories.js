3b42e69400db1c14f2e4df7dab83b2b1f1211335
Jesse Brack <jtbrack@gmail.com>
2019-07-12 14:08:48 -0400

    chore(color-picker): add Storybook entries (#3922)

---
+++ ui/components/color-picker/index.stories.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import * as BaseExamples from './base/example';
+import * as CustomExamples from './custom-only/example';
+import * as PredefinedExamples from './predefined-only/example';
+import * as SwatchesExamples from './swatches-only/example';
+import { getAllDisplayCollectionsByType } from '../../shared/helpers';
+import StoryFrame from '../../../shared/components/StoryFrame';
+
+import '../../index.scss';
+
+const examples = [
+  BaseExamples,
+  CustomExamples,
+  PredefinedExamples,
+  SwatchesExamples
+];
+
+const kitchenSink = getAllDisplayCollectionsByType(examples, [
+  'default',
+  'examples',
+  'states'
+]);
+
+storiesOf('Components/Color Picker', module).add('Kitchen Sink', () =>
+  kitchenSink.map((element, idx) =>
+    element.map(({ label, component }) => (
+      <StoryFrame
+        component={component}
+        label={label}
+        key={`kitchen-sink-${label}-${idx}`}
+      />
+    ))
+  )
+);

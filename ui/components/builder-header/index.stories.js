b0049d1e0bc38f533f5cf04715b1abaec5aba46e
Jesse Brack <jtbrack@gmail.com>
2019-07-13 06:34:51 -0400

    chore(builder-header): add Storybook entries (#3917)

---
+++ ui/components/builder-header/index.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import * as BaseExamples from './base/example';
+import * as ToolbarExamples from './toolbar/example';
+import { getAllDisplayCollectionsByType } from '../../shared/helpers';
+import StoryFrame from '../../../shared/components/StoryFrame';
+
+import '../../index.scss';
+
+const examples = [BaseExamples, ToolbarExamples];
+
+const kitchenSink = getAllDisplayCollectionsByType(examples, [
+  'default',
+  'examples',
+  'states'
+]);
+
+storiesOf('Components/Builder Header', module).add('Kitchen Sink', () =>
+  kitchenSink.map((element, idx) =>
+    element.map(({ label, component }) => (
+      <StoryFrame
+        component={component}
+        label={label}
+        styles={`height: 100px;`}
+        key={`kitchen-sink-${label}-${idx}`}
+      />
+    ))
+  )
+);

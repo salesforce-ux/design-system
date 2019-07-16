4fbee37973b49688cccd7c6c099356cc4efeeb4d
Jesse Brack <jtbrack@gmail.com>
2019-07-16 19:16:27 -0400

    chore(progress-bar): add Storybook entries (#3940)

---
+++ ui/components/progress-bar/index.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import * as BaseExamples from './base/example';
+import * as VerticalExamples from './vertical/example';
+import { getAllDisplayCollectionsByType } from '../../shared/helpers';
+import StoryFrame from '../../../shared/components/StoryFrame';
+
+import '../../index.scss';
+
+const examples = [BaseExamples, VerticalExamples];
+
+const kitchenSink = getAllDisplayCollectionsByType(examples, [
+  'default',
+  'examples',
+  'states'
+]);
+
+storiesOf('Components/Progress Bar', module).add('Kitchen Sink', () =>
+  kitchenSink.map((element, idx) =>
+    element.map(({ label, component }) => (
+      <StoryFrame
+        component={component}
+        label={label}
+        styles={`height: 6rem`}
+        key={`kitchen-sink-${label}-${idx}`}
+      />
+    ))
+  )
+);

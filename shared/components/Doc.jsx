a5f3c59fe08506f4284028cef2bfec631b810072
DouG Molidor <dmolidor@salesforce.com>
2019-08-16 13:47:30 -0700

    fix(docs): Create functioning deep linking to doc headers (#4005)
    * add DocHeaderAnchor to handles deep links into doc block content
    
    * update deep links across docs

--- shared/components/Doc.jsx
+++ shared/components/Doc.jsx
@@ -9,18 +9,7 @@ import '../styles/doc.scss';
 import { flattenElement, mapElement } from '../utils/react';
 
 import Example from './Example';
-
-export const createAnchor = (type, id) =>
-  React.createElement(
-    'a',
-    {
-      'aria-label': 'Jump to section',
-      className: 'doc doc-anchor',
-      'data-level': type[1],
-      href: `#${id}`
-    },
-    '#'
-  );
+import DocHeaderAnchor from './DocHeaderAnchor';
 
 export const tags = [
   'p',
@@ -53,7 +42,16 @@ export const factories = tags.reduce(
     Object.assign({}, factories, {
       [type]: (props, ...children) => {
         if (/h[1-6]/.test(type)) {
-          children.push(createAnchor(type, props.id));
+          children.push(
+            React.createElement(
+              DocHeaderAnchor,
+              {
+                id: props.id,
+                type
+              },
+              null
+            )
+          );
         }
         return React.createElement(
           type,
@@ -78,7 +76,9 @@ export const createTableOfContents = (() => {
       title:
         typeof children === 'string'
           ? children
-          : Array.isArray(children) ? children[0] : children
+          : Array.isArray(children)
+          ? children[0]
+          : children
     };
   };
   return element => {

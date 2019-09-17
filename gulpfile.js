286bc0f1ca742ffecf8ca45da8b88be1dd08a7a8
Jade Pennig <jade@pennig.name>
2019-09-17 16:41:35 -0500

    chore(storybook-docs): tokens need to be generated first for ui.json build

--- gulpfile.js
+++ gulpfile.js
@@ -224,6 +224,7 @@ gulp.task('build', gulp.series('clean', 'generate:tokens:all', 'styles'));
 gulp.task(
   'dist:ui-json',
   gulp.series(
+    'generate:tokens:all',
     withName('dist:clean:before')(dist.cleanBefore),
     withName('dist:writeUI')(dist.writeUI),
     withName('dist:clean:after')(dist.cleanAfter)

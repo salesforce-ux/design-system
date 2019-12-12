08ebab2aa7d2efcbc3a9c43aef6f5126726a884f
Jesse Brack <jtbrack@gmail.com>
2019-12-12 13:59:22 -0800

    chore(badge): clean up inconsistent story naming (#4116)

--- ui/modules/badge/base/index.stories.js
+++ ui/modules/badge/base/index.stories.js
@@ -2,10 +2,10 @@ import { storiesOf } from '@storybook/react';
 import * as Examples from './example';
 import { getDisplayElementById } from '../../../shared/helpers';
 
-storiesOf('Modules/Badges/Base', module).add('Base', () => Examples.default);
+storiesOf('Modules/Badge/Base', module).add('Base', () => Examples.default);
 
 Examples.examples.forEach(example => {
-  storiesOf('Modules/Badges/Base', module).add(example.label, () =>
+  storiesOf('Modules/Badge/Base', module).add(example.label, () =>
     getDisplayElementById(Examples.examples, example.id)
   );
 });

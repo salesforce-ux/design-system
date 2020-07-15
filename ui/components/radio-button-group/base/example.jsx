c8b2afa17112504fda73f363f7f9b37a96d8a1ea
Lise LaTorre <llatorre@salesforce.com>
2020-07-15 12:26:20 -0700

    fix(radio-button-group): update error messge ID to use uniqueId

--- ui/components/radio-button-group/base/example.jsx
+++ ui/components/radio-button-group/base/example.jsx
@@ -48,7 +48,7 @@ const ExampleRadioButtonGroup = ({
   hasError
 }) => {
   const exampleName = uniqueId('example-unique-name-');
-  const exampleErrorId = 'error_01';
+  const exampleErrorId = uniqueId('error-unique-id-');
 
   return (
     <Fieldset className={hasError && 'slds-has-error'}>

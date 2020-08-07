fb21f12aa20eeed98561192d91959782d8635e6e
Jade Pennig <jade@pennig.name>
2020-08-06 20:32:42 -0500

    feat(css-vars-extract): update table style and row contents

--- shared/components/StyleApiTable.jsx
+++ shared/components/StyleApiTable.jsx
@@ -4,10 +4,35 @@
 import React, { Component } from 'react';
 import { propTypes } from '../../scripts/var-metadata';
 
+const getPropTypeData = (name, varName) => {
+  const trimmedVar = varName.replace(`--sds-c-${name}-`, '');
+  let propType = null;
+  let valueTypes = null;
+
+  Object.keys(propTypes).forEach(prop => {
+    if (trimmedVar.match(prop)) {
+      propType = propTypes[prop].type;
+      valueTypes = propTypes[prop].valueTypes;
+    }
+  });
+
+  return {
+    propType,
+    valueTypes
+  };
+};
+
 class StyleApiTable extends Component {
   render() {
     const { name, type } = this.props;
     const vars = require(`../../.generated/metadata/${type}s/${name}/styling-hooks.json`);
+
+    const categories = Object.keys(vars)
+      .map(varName => {
+        return getPropTypeData(name, varName).propType;
+      })
+      .filter((value, i, self) => self.indexOf(value) === i)
+      .sort();
 
     return (
       <div>
@@ -18,37 +43,51 @@ class StyleApiTable extends Component {
           </p>
         </div>
 
-        <table className="slds-table slds-table_bordered">
+        <table className="slds-table">
           <thead>
             <tr>
-              <th>Style API Custom Property</th>
-              <th>Property Category</th>
+              <th>Styling Hook Name</th>
               <th>Value Type(s)</th>
+              <th>Fallback Value</th>
             </tr>
           </thead>
           <tbody>
-            {Object.keys(vars).map((varName, i) => {
-              const trimmedVar = varName.replace(`--sds-c-${name}-`, '');
-              let propType = null;
-              let valueTypes = null;
-
-              Object.keys(propTypes).forEach(prop => {
-                if (trimmedVar.match(prop)) {
-                  propType = propTypes[prop].type;
-                  valueTypes = propTypes[prop].valueTypes;
-                }
-              });
+            {categories.map((category, i) => {
+              const categoryVars = Object.keys(vars)
+                .filter(
+                  varName =>
+                    getPropTypeData(name, varName).propType === category
+                )
+                .map(varName => {
+                  return {
+                    name: varName,
+                    value: vars[varName],
+                    types: getPropTypeData(name, varName).valueTypes || []
+                  };
+                });
 
               return (
-                <tr key={`component-var-${i}`}>
-                  <td>{varName}</td>
-                  <td>{propType}</td>
-                  <td>
-                    {valueTypes
-                      ? valueTypes.reduce((a, b) => `${a}, ${b}`)
-                      : null}
-                  </td>
-                </tr>
+                <>
+                  <tr key={`${category}-${i}`}>
+                    <td colspan="3">{category}</td>
+                  </tr>
+                  {categoryVars.map((varData, ii) => (
+                    <tr key={`${category}-${i}-${ii}`}>
+                      <td>{varData.name}</td>
+                      <td>
+                        {varData.types.map((type, x) => (
+                          <a
+                            key={`${category}-${i}-${ii}-${x}`}
+                            href={`https://developer.mozilla.org/en-US/docs/Web/CSS/${type.toLowerCase()}`}
+                          >
+                            {type}
+                          </a>
+                        ))}
+                      </td>
+                      <td>{varData.value}</td>
+                    </tr>
+                  ))}
+                </>
               );
             })}
           </tbody>

8b4fbb6555285341feb22fc30a2bd61e674bc382
Jesse Brack <jbrack@salesforce.com>
2020-09-01 16:08:51 -0700

    fix(codeblock): Update codeblock's rendering behavior (#4405)

--- shared/components/CodeBlock.jsx
+++ shared/components/CodeBlock.jsx
@@ -42,7 +42,7 @@ class CodeBlock extends Component {
   }
 
   getCode(lines) {
-    const { children } = this.props;
+    const { toggleCode, children } = this.props;
     try {
       React.Children.only(children);
     } catch (error) {
@@ -54,7 +54,8 @@ class CodeBlock extends Component {
     const formatted = beautify(markup);
     let code;
 
-    if (lines) {
+    // Only reduce if # of lines is specified and the codeblock is meant to be toggled
+    if (lines && toggleCode) {
       code = formatted
         .split(/\n/g)
         .reduce((codeLines, codeLine, i) =>
@@ -73,7 +74,7 @@ class CodeBlock extends Component {
   }
 
   render() {
-    const { language, toggleCode = true } = this.props;
+    const { language, toggleCode } = this.props;
     const { open } = this.state;
     const codeLines = open ? null : 3;
 
@@ -119,7 +120,8 @@ CodeBlock.propTypes = {
 };
 
 CodeBlock.defaultProps = {
-  language: 'html'
+  language: 'html',
+  toggleCode: true
 };
 
 export default CodeBlock;

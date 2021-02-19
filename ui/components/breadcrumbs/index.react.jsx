f88a806fa49e794ee25219a5f88962eba013be19
Doug Molidor <dmolidor@salesforce.com>
2021-02-18 17:56:34 -0800

    fix linting

--- ui/components/breadcrumbs/index.react.jsx
+++ ui/components/breadcrumbs/index.react.jsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
 // Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
 
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import classNames from 'classnames';
 
 const Crumb = ({

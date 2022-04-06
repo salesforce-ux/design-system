9b527ae070b51fe637b9fbb2467f81f6b3679af6
Kim Flournoy <53910308+kflournoy-sf@users.noreply.github.com>
2022-04-06 18:23:40 -0400

    bug(prompt): revert syntax change to fix broken page (#5041)
    Co-authored-by: DouG Molidor <dmolidor@salesforce.com>

--- ui/components/prompt/base/example.jsx
+++ ui/components/prompt/base/example.jsx
@@ -10,48 +10,42 @@ import {
   Backdrop
 } from '../../modals/base/example';
 
-export default [
-  {
-    id: 'default',
-    label: 'Default',
-    element: (
-      <div className="demo-only" style={{ height: '24rem' }}>
-        <Backdrop>
-          <Modal
-            tabIndex="0"
-            role="alertdialog"
-            aria-labelledby="prompt-heading-id"
-            aria-describedby="prompt-message-wrapper"
-            className="slds-modal_prompt"
-            closeButton={false}
-          >
-            <ModalHeader
-              className="slds-theme_error slds-theme_alert-texture"
-            >
-              <h1 className="slds-text-heading_medium" id="prompt-heading-id">
-                Service unavailable
-              </h1>
-            </ModalHeader>
-            <ModalContent
-              className="slds-p-around_medium"
-              id="prompt-message-wrapper"
-            >
-              <p>
-                Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco
-                deserunt aute id consequat veniam incididunt duis in sint irure nisi.
-                Mollit officia cillum Lorem ullamco minim nostrud elit officia tempor
-                esse quis. Cillum sunt ad dolore quis aute consequat ipsum magna
-                exercitation reprehenderit magna. Tempor cupidatat consequat elit
-                dolor adipisicing.
-              </p>
-            </ModalContent>
+export default (
+  <div className="demo-only" style={{ height: '24rem' }}>
+    <Backdrop>
+      <Modal
+        tabIndex="0"
+        role="alertdialog"
+        aria-labelledby="prompt-heading-id"
+        aria-describedby="prompt-message-wrapper"
+        className="slds-modal_prompt"
+        closeButton={false}
+      >
+        <ModalHeader
+          className="slds-theme_error slds-theme_alert-texture"
+        >
+          <h1 className="slds-text-heading_medium" id="prompt-heading-id">
+            Service unavailable
+          </h1>
+        </ModalHeader>
+        <ModalContent
+          className="slds-p-around_medium"
+          id="prompt-message-wrapper"
+        >
+          <p>
+            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco
+            deserunt aute id consequat veniam incididunt duis in sint irure nisi.
+            Mollit officia cillum Lorem ullamco minim nostrud elit officia tempor
+            esse quis. Cillum sunt ad dolore quis aute consequat ipsum magna
+            exercitation reprehenderit magna. Tempor cupidatat consequat elit
+            dolor adipisicing.
+          </p>
+        </ModalContent>
 
-            <ModalFooter className="slds-theme_default">
-              <button className="slds-button slds-button_neutral">Got It</button>
-            </ModalFooter>
-          </Modal>
-        </Backdrop>
-      </div>
-    )
-  }
-];
+        <ModalFooter className="slds-theme_default">
+          <button className="slds-button slds-button_neutral">Got It</button>
+        </ModalFooter>
+      </Modal>
+    </Backdrop>
+  </div>
+);

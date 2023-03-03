3fcf5af45f0873c5caf4308a984ad11e8b6c2c0b
Amarnath Chandana <amarnath.chandana@outlook.com>
2023-03-03 19:26:34 +0530

    feat(stylinghooks): create transparent styling hooks (#5161)
    * feat(stylinghooks): create transparent styling hooks
    
    * chore: add zero opacity
    
    * chore: update comment in transparent styling hooks
    
    * chore: add transparencies only for neutral 10/100
    
    * chore: update comment

--- scripts/gulp/generate/tokens.js
+++ scripts/gulp/generate/tokens.js
@@ -37,29 +37,6 @@ let formatTransforms = _({
   .flatten()
   .value();
 
-const formatsForSdsProps = [
-  {
-    format: 'json',
-    transform: 'web',
-  },
-  {
-    format: 'common.js',
-    transform: 'web',
-  },
-  {
-    format: 'module.js',
-    transform: 'web',
-  },
-  {
-    format: 'custom-props.scss',
-    transform: 'raw',
-  },
-  {
-    format: 'raw.json',
-    transform: 'raw',
-  }
-];
-
 // Pipe through every YAML file and generate a platform specific token file
 export const packages = (done) => {
   const convert = ({ name, transform }, done) =>
@@ -74,21 +51,6 @@ export const packages = (done) => {
       .pipe(gulp.dest(path.resolve(paths.designTokens, 'dist')))
       .on('finish', done);
   async.each(formatTransforms, convert, done);
-};
-
-export const stylingHooks = (done) => {
-  const convert = ({ format, transform }, done) =>
-    gulp
-      .src(path.resolve(paths.sdsStylingHooksProps, '*.json'))
-      .pipe(
-        gulpTheo({
-          transform: { type: transform },
-          format: { type: format }
-        })
-      )
-      .pipe(gulp.dest(path.resolve(paths.designTokens, 'dist')))
-      .on('finish', done);
-  async.each(formatsForSdsProps, convert, done);
 };
 
 // Pipe through every component specific YAML file and generate a file of imports
